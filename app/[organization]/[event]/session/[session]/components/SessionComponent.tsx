f013029409951225111e315d85bbfe5c63e4d3c6
xvoorvaa <xander@xvproductions.com>
2023-08-02 14:56:30 +0200

    Speaker is now sticky

--- app/[organization]/[event]/session/[session]/components/SessionComponent.tsx
+++ app/[organization]/[event]/session/[session]/components/SessionComponent.tsx
@@ -7,7 +7,7 @@ import SpeakerIconList from "@/components/speakers/SpeakerIconList";
 const SpeakerComponent = ({ session }: { session: Session }) => {
   return (
     <ComponetCard title="Speakers">
-      <SpeakerIconList speakers={session.speakers}  />
+      <SpeakerIconList speakers={session.speakers} />
     </ComponetCard>
   );
 };
@@ -18,7 +18,7 @@ export default async function SessionComponent({
 }) {
   return (
     <div className="flex flex-col w-full max-h-full h-full lg:flex-row relative overflow-y-scroll">
-      <div className="flex flex-col w-full lg:flex-grow lg:h-full lg:w-[70%] lg:overflow-y-scroll box-border">
+      <div className="flex flex-col w-full lg:flex-grow lg:h-full lg:w-[70%] box-border">
         <div className="lg:h-3/4 w-full lg:p-4 pb-2 lg:pr-2">
           <Player playbackId={session.playbackId} playerName={session.name} />
         </div>
@@ -26,7 +26,7 @@ export default async function SessionComponent({
           <SessionInfoBox session={session.toJson()} />
         </div>
       </div>
-      <div className="flex flex-col w-full lg:w-[30%] p-4 pt-2 lg:pt-4 lg:p-2 lg:overflow-y-scroll">
+      <div className="flex flex-col w-full lg:sticky lg:top-0 lg:w-[30%] p-4 pt-2 lg:pt-4 lg:p-2 lg:overflow-y-scroll">
         <SpeakerComponent session={session} />
         <div className="rounded border-2 cursor-pointer border-accent font-bold uppercase text-center p-4 mt-4 shadow hover:bg-accent  bg-accent text-white hover:animate-pulse">
           Collect this talk
