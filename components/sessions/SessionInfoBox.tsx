0574bb6d13f8a77b192871fac8b5f3fdbd1a44b9
xvoorvaa <xander@xvproductions.com>
2023-08-02 14:20:18 +0200

    Put the Date next to the Title of the "Video Card"
    Date is now next to the Title, and I made the formatting of the Description "justify"
    Also fixed typo

--- components/sessions/SessionInfoBox.tsx
+++ components/sessions/SessionInfoBox.tsx
@@ -22,27 +22,27 @@ const SessionInfoBox = ({ session }: { session: ISession | undefined }) => {
   }
   return (
     <div className="flex flex-col w-full h-full ">
-    <ComponetCard title={session.name}>
-      <div className="flex flex-col md:flex-row w-full h-full ">
-        <p className="text-main-text md:text-lg">{session.description}</p>
-        <div className="flex w-full md:flex-col md:w-1/4 md:ml-auto gap-2">
-          <p className="p-1 w-full md:text-lg text-secondary-text text-left mt-auto md:text-right">
-            {session.start.toDateString()}
+      <ComponetCard title={session.name} date={session.start}>
+        <div className="flex flex-col md:flex-row w-full h-full ">
+          <p className="text-main-text md:text-lg text-justify">
+            {session.description}
           </p>
-          <div className="flex flex-row md:w-full pt-4 mt-auto">
-            <CodeBracketIcon
-              className="p-1 cursor-pointer ml-auto h-8 w-8 text-accent font-medium"
-              onClick={() => {
-                modal.openModal(
-                  <EmbedSessionModal stageId={session.stageId} />
-                );
-              }}
-            />
-            <ShareIcon className="p-1 h-8 w-8 cursor-pointer ml-3 text-accent" />
+          <div className="flex w-full md:flex-col md:w-1/4 md:ml-auto gap-2">
+            <p className="p-1 w-full md:text-lg text-secondary-text text-left mt-auto md:text-right"></p>
+            <div className="flex flex-row md:w-full pt-4 mt-auto">
+              <CodeBracketIcon
+                className="p-1 cursor-pointer ml-auto h-8 w-8 text-accent font-medium"
+                onClick={() => {
+                  modal.openModal(
+                    <EmbedSessionModal stageId={session.stageId} />
+                  );
+                }}
+              />
+              <ShareIcon className="p-1 h-8 w-8 cursor-pointer ml-3 text-accent" />
+            </div>
           </div>
         </div>
-      </div>
-    </ComponetCard>
+      </ComponetCard>
     </div>
   );
 };
