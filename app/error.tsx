2e20e5bb1936f59b150a472f150eacea71abf50c
xvoorvaa <xander@xvproductions.com>
2023-08-03 16:04:12 +0200

    Made files prettier

--- app/error.tsx
+++ app/error.tsx
@@ -1,19 +1,13 @@
 'use client' // Error components must be Client Components
- 
+
 import { useEffect } from 'react'
- 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error
-  reset: () => void
-}) {
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
- 
+
   return (
     <div>
       <h2>Something went wrong!</h2>
@@ -21,8 +15,7 @@ export default function Error({
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
-        }
-      >
+        }>
         Try again
       </button>
     </div>
