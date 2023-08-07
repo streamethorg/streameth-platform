dd38c28a59a28a44af5638ca675fb3a2c7e06a64
xvoorvaa <xander@xvproductions.com>
2023-08-07 14:49:36 +0200

    Zuzalu Public Goods

--- server/test.ts
+++ server/test.ts
@@ -14,17 +14,17 @@ const run = async () => {
   const organizationInstance = await orgController.createOrganization(Organization)
 
   const Event = {
-    name: 'Zuzalu 2023 - New Cities & Network States',
+    name: 'Zuzalu 2023 - Public Goods',
     description: 'Zuzalu is a first-of-its-kind pop-up city community in Montenegro.',
-    start: new Date('2023-04-30T00:00:00.000Z'), // "2021-09-30T00:00:00.000Z
-    end: new Date('2023-05-01T00:00:00.000Z'), // "2021-10-02T00:00:00.000Z
+    start: new Date('2023-04-12T00:00:00.000Z'), // "2021-09-30T00:00:00.000Z
+    end: new Date('2023-04-15T00:00:00.000Z'), // "2021-10-02T00:00:00.000Z
     location: 'Montenegro',
     organizationId: organizationInstance.id,
     dataImporter: [
       {
         type: 'gsheet' as 'gsheet',
         config: {
-          sheetId: '1uT9UNYJiMJLaPjBwtEHfoUMXaGy8ZD9cTN2LN4dlydc',
+          sheetId: '1DtKqSLXTw9ujLcNPmA26MY6UuiQm75dol2Hh5xH6Tkc',
           apiKey: 'AIzaSyChBCoGLIXhlMxY3eI9gJMpYujvFN90v6w',
         },
       },
