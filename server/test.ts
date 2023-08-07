45cc579d59219043bde64612ed35f6d4f63dfa39
xvoorvaa <xander@xvproductions.com>
2023-08-07 14:33:34 +0200

    Zuzalu zkSync

--- server/test.ts
+++ server/test.ts
@@ -14,10 +14,10 @@ const run = async () => {
   const organizationInstance = await orgController.createOrganization(Organization)
 
   const Event = {
-    name: 'Zuzalu Montenegro - zkSync',
+    name: 'Zuzalu 2023 - zkSync',
     description: 'Zuzalu is a first-of-its-kind pop-up city community in Montenegro.',
-    start: new Date('2023-03-17T00:00:00.000Z'), // "2021-09-30T00:00:00.000Z
-    end: new Date('2023-05-23T00:00:00.000Z'), // "2021-10-02T00:00:00.000Z
+    start: new Date('2023-04-16T00:00:00.000Z'), // "2021-09-30T00:00:00.000Z
+    end: new Date('2023-04-23T00:00:00.000Z'), // "2021-10-02T00:00:00.000Z
     location: 'Montenegro',
     organizationId: organizationInstance.id,
     dataImporter: [
@@ -25,7 +25,7 @@ const run = async () => {
         type: 'gsheet' as 'gsheet',
         config: {
           sheetId: '1HlHRmoM5zn67iAX9_JJSpK6NNVQX5dshhPUzilRXipA',
-          apiKey: 'AIzaSyBfg-L5lDCUx7L-s6ubJe3z6mYeXIkIFU4',
+          apiKey: 'AIzaSyChBCoGLIXhlMxY3eI9gJMpYujvFN90v6w',
         },
       },
     ],
