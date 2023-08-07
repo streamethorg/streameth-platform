f8a311c1f025dc149cd49ba08017dd3cc775461c
xvoorvaa <xander@xvproductions.com>
2023-08-07 14:53:04 +0200

    Zuzalu Synthetic Biology

--- server/test.ts
+++ server/test.ts
@@ -14,17 +14,17 @@ const run = async () => {
   const organizationInstance = await orgController.createOrganization(Organization)
 
   const Event = {
-    name: 'Zuzalu 2023 - Jurisdictions for Longevity',
+    name: 'Zuzalu 2023 - Synthetic Biology',
     description: 'Zuzalu is a first-of-its-kind pop-up city community in Montenegro.',
-    start: new Date('2023-04-09T00:00:00.000Z'), // "2021-09-30T00:00:00.000Z
-    end: new Date('2023-04-11T00:00:00.000Z'), // "2021-10-02T00:00:00.000Z
+    start: new Date('2023-04-01T00:00:00.000Z'), // "2021-09-30T00:00:00.000Z
+    end: new Date('2023-04-02T00:00:00.000Z'), // "2021-10-02T00:00:00.000Z
     location: 'Montenegro',
     organizationId: organizationInstance.id,
     dataImporter: [
       {
         type: 'gsheet' as 'gsheet',
         config: {
-          sheetId: '1KYyLZGlO1nLJYbTzQbUvOX_KrRAhYYwZdhGIVE1kwD4',
+          sheetId: '1cKin8UyfylwbrNcU_4r0TldAaFDdv5yvhyU1WQuoSqQ',
           apiKey: 'AIzaSyChBCoGLIXhlMxY3eI9gJMpYujvFN90v6w',
         },
       },
