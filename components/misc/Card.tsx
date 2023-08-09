0764b7ce556b657859242599ccb68bd0f5763e9c
pabloVoorvaart <me@pablovoorvaart.me>
2023-08-09 13:02:05 +0200

    prettier

--- components/misc/Card.tsx
+++ components/misc/Card.tsx
@@ -1,7 +1,5 @@
 const Card = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-col rounded-md h-full p-4 shadow box-border bg-base">
-    {children}
-  </div>
-);
+  <div className="flex flex-col rounded-md h-full p-4 shadow box-border bg-base">{children}</div>
+)
 
-export default Card;
+export default Card
