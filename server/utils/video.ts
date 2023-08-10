d489df1d20209c5e8a8e2abfc99b84c312334260
Xander <56308735+xvoorvaa@users.noreply.github.com>
2023-08-10 19:29:41 +0200

    Added new thumbnails & JSON Files (#16)

--- server/utils/video.ts
+++ server/utils/video.ts
@@ -1,35 +1,28 @@
-import ffmpeg from "fluent-ffmpeg";
-import axios from "axios";
-import fs from "fs";
+import ffmpeg from 'fluent-ffmpeg'
+import axios from 'axios'
+import fs from 'fs'
 
 export async function extractFirstFrame(hlsUrl: string, filePath: string) {
   // Check if the file already exists
   if (fs.existsSync(filePath)) {
-    console.log("File already exists, skipping frame extraction.");
-    return;
+    console.log('File already exists, skipping frame extraction.')
+    return
   }
-  // replace index.m3u8 with 720p0/index.m3u8
-  // hlsUrl = hlsUrl.replace("index.m3u8", "index1080p0.m3u8");
-  let response = await axios.get(hlsUrl);
 
-  const body = response.data;
-  const lines = body.split("\n");
-  const tsUrl = lines.find((line: string) => line.endsWith(".ts"));
-
-  // if (!tsUrl) {
-  //   throw new Error("No .ts URL found in HLS stream");
-  // }
-ffmpeg(hlsUrl)
-.screenshots({
-  timemarks: ['00:00:06.000'],
-  filename: filePath, 
-  folder: '.',
-  size: '1920x1080'
-})
-.on('end', function() {
-  console.log('Screenshots taken');
-})
-.on('error', function(err) {
-  console.log('An error occurred: ' + err.message);
-});
+  ffmpeg(hlsUrl)
+    .screenshots({
+      timemarks: ['00:00:06.000'],
+      filename: filePath,
+      folder: '.',
+      size: '1920x1080',
+    })
+    .on('end', function () {
+      console.log('Screenshots taken')
+    })
+    .on('error', function (err) {
+      console.log('An error occurred: ' + err.message)
+    })
+    .on('stderr', function (stderrLine) {
+      console.log('Stderr: ' + stderrLine)
+    })
 }
