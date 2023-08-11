912fb2e39e5d39e4c02902fe7a1f26b639ab2013
Xander <56308735+xvoorvaa@users.noreply.github.com>
2023-08-11 18:27:54 +0200

    Zuzalu JSON Files and thumbnails (#17)
    * Added new thumbnails & JSON Files
    
    * Added Zuzalu - zkSync JSON- and thumbnail files
    
    * Added Zuzalu - Synthetic Bio JSON- and thumbnail files
    
    * Added Zuzalu - Public Goods JSON- and thumbnail files
    
    * Added Zuzalu - Network States JSON- and thumbnail files
    
    * Added Zuzalu - Longevity Intro JSON- and thumbnail files
    
    * Added Zuzalu - Longevity Biotech Conference Intro JSON- and thumbnail files
    
    * Added Zuzalu - Longevity 0-1 JSON- and thumbnail files
    
    * Added Zuzalu - Longevity Jurisdiction JSON- and thumbnail files
    
    * Added Zuzalu - Crypto x AI JSON- and thumbnail files
    
    * Prettier ✨
    
    * Added Zuzalu - Other JSON- and thumbnail files
    
    * Changed thumbnail to just one image
    
    * Prettier ✨
    
    * Added Cover Images
    
    * Changed prettier command
    
    * Changed dates

---
+++ server/utils/change_thumbnail.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+const directoryPath = path.join(__dirname, '../../data/sessions/')
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    return console.log('Unable to scan directory:', err)
+  }
+
+  files.forEach((file) => {
+    if (file.startsWith('zuzalu_montenegro_2023')) {
+      const fullPath = path.join(directoryPath, file)
+
+      fs.readdir(fullPath, (err, jsonFiles) => {
+        if (err) {
+          return console.log('Unable to scan subdirectory:', err)
+        }
+
+        jsonFiles.forEach((jsonFile) => {
+          if (jsonFile.endsWith('.json')) {
+            const jsonPath = path.join(fullPath, jsonFile)
+
+            fs.readFile(jsonPath, 'utf8', (err, data) => {
+              if (err) {
+                return console.log('Unable to read file:', err)
+              }
+
+              let jsonData = JSON.parse(data)
+              jsonData.coverImage = '/sessions/zuzalu_montenegro_2023/zuzalu_thumbnail.jpg'
+
+              fs.writeFile(jsonPath, JSON.stringify(jsonData, null, 2), 'utf8', (err) => {
+                if (err) {
+                  return console.log('Unable to write file:', err)
+                }
+                console.log(`Updated coverImage in ${jsonFile}`)
+              })
+            })
+          }
+        })
+      })
+    }
+  })
+})
