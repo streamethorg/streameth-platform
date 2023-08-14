d828bed981213fcbb0abaf0deac948a2692ff449
xvoorvaa <xander@xvproductions.com>
2023-08-14 17:04:48 +0200

    Fixed bug of the session `title` and `date` overlapping over each other
    Also removed some unused `imports` and  a 2px padding to mobile speaker card

--- app/[organization]/[event]/session/[session]/components/SessionComponent.tsx
+++ app/[organization]/[event]/session/[session]/components/SessionComponent.tsx
@@ -24,7 +24,7 @@ export default async function SessionComponent({ session }: { session: Session }
           <SessionInfoBox session={session.toJson()} />
         </div>
       </div>
-      <div className="flex flex-col w-full pb-2 lg:w-[30%] lg:overflow-y-scroll">
+      <div className="flex flex-col w-full p-2 lg:w-[30%] lg:overflow-y-scroll lg:p-0">
         <SpeakerComponent session={session} />
         <div className="rounded border-2 cursor-pointer border-accent font-bold uppercase text-center p-4 mt-4 shadow hover:bg-accent  bg-accent text-white hover:animate-pulse">
           Collect this talk
