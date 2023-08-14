6a0e031d4d78064f2324883ec6eba4d5c5c5d382
xvoorvaa <xander@xvproductions.com>
2023-08-14 18:44:37 +0200

    Reverted hover effect

--- components/misc/Card.tsx
+++ components/misc/Card.tsx
@@ -1,8 +1,5 @@
 const Card = ({ children, isAvailable = true }: { children: React.ReactNode; isAvailable?: boolean }) => (
-  <div
-    className={`flex flex-col rounded-md h-full p-4 shadow box-border ${isAvailable ? 'cursor-pointer hover:bg-gray-300 bg-base' : 'bg-gray-200'}`}>
-    {children}
-  </div>
+  <div className={`flex flex-col rounded-md h-full p-4 shadow box-border ${isAvailable ? 'cursor-pointer bg-base' : 'bg-gray-200'}`}>{children}</div>
 )
 
 export default Card
