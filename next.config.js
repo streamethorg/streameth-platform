c335c4ee4f5404480cc34df973271bf35b72582b
xvoorvaa <xander@xvproductions.com>
2023-08-16 00:11:43 +0200

    Testing CORS

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://livepeer.studio',
+          },
+        ],
+      },
+    ]
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
       new webpack.DefinePlugin({
-        'process.env.FLUENTFFMPEG_COV': false
-    })
+        'process.env.FLUENTFFMPEG_COV': false,
+      })
     )
- 
+
     return config
   },
   images: {
