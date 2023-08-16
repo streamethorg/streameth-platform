96b18133ee871323f03070c3a426db02d7677faa
xvoorvaa <xander@xvproductions.com>
2023-08-16 19:05:41 +0200

    Temporary fix for the long `<Card>`

--- app/[organization]/[event]/archive/components/ArchivedSession.tsx
+++ app/[organization]/[event]/archive/components/ArchivedSession.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { useState } from 'react'
 import Link from 'next/link'
 import Card from '@/components/misc/Card'
+
 const ArchivedSession = ({ session, learnMore = false, goToStage = false }: { session: ISession; learnMore?: boolean; goToStage?: boolean }) => {
   const [image, setImage] = useState(session.coverImage)
   const alt = '/events/' + session.eventId + '.png'
@@ -28,11 +29,12 @@ const ArchivedSession = ({ session, learnMore = false, goToStage = false }: { se
           }}
         />
       </div>
-      <p className="border-b-2 border-accent  p-2 py-4 flex flex-grow text-md ">{session.name}</p>
+      <p className="border-b-2 border-accent p-2 py-4 flex flex-grow text-md ">{session.name}</p>
     </Card>
   )
   if (learnMore) return <Link href={'session/' + session.id}>{component}</Link>
   if (goToStage) return <Link href={'/stage/' + session.stageId}>{component}</Link>
+
   return component
 }
 
--- app/[organization]/[event]/archive/components/FilteredItems.tsx
+++ app/[organization]/[event]/archive/components/FilteredItems.tsx
@@ -7,7 +7,8 @@ import { ISession } from '@/server/model/session'
 const FilteredItems = () => {
   const { filteredItems } = useContext(FilterContext)
   return (
-    <div className="h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4 md:overflow-scroll">
+    <div
+      className="h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4 md:overflow-scroll items-start">
       {filteredItems.map((session: ISession) => {
         return <ArchivedSession session={session} key={session.id} learnMore />
       })}
