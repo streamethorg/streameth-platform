f4be49ed08ce964c58e0e5112e9b137972e57ace
xvoorvaa <xander@xvproductions.com>
2023-08-16 19:09:13 +0200

    Le Prettier ✨

--- app/[organization]/[event]/archive/components/FilteredItems.tsx
+++ app/[organization]/[event]/archive/components/FilteredItems.tsx
@@ -7,8 +7,7 @@ import { ISession } from '@/server/model/session'
 const FilteredItems = () => {
   const { filteredItems } = useContext(FilterContext)
   return (
-    <div
-      className="h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4 md:overflow-scroll items-start">
+    <div className="h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4 md:overflow-scroll items-start">
       {filteredItems.map((session: ISession) => {
         return <ArchivedSession session={session} key={session.id} learnMore />
       })}
