71e0133a047ef4aedca969947244ad7faa6d791a
xvoorvaa <xander@xvproductions.com>
2023-08-16 15:39:18 +0200

    Removed "Collect this talk"

--- app/[organization]/[event]/session/[session]/components/SessionComponent.tsx
+++ app/[organization]/[event]/session/[session]/components/SessionComponent.tsx
@@ -26,9 +26,6 @@ export default async function SessionComponent({ session }: { session: Session }
       </div>
       <div className="flex flex-col w-full p-2 lg:w-[30%] lg:overflow-y-scroll lg:p-0">
         <SpeakerComponent session={session} />
-        <div className="rounded border-2 cursor-pointer border-accent font-bold uppercase text-center p-4 mt-4 shadow hover:bg-accent  bg-accent text-white hover:animate-pulse">
-          Collect this talk
-        </div>
       </div>
     </div>
   )
