15a760b1e4cd64e9d1164372bcd46ca7f2d0a2d0
wslyvh <wslyvh@users.noreply.github.com>
2023-08-30 14:56:08 +0200

    admin siwe auth

--- components/context/GeneralContext.tsx
+++ components/context/GeneralContext.tsx
@@ -1,27 +1,7 @@
 'use client'
 import { useMemo } from 'react'
 import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react'
-// import { Analytics } from '@vercel/analytics/react'
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { createConfig, configureChains, mainnet, WagmiConfig } from 'wagmi'
-import { publicProvider } from '@wagmi/core/providers/public'
-
-import '@rainbow-me/rainbowkit/styles.css'
 import { Analytics } from '@vercel/analytics/react'
-
-const { chains, publicClient, webSocketPublicClient } = configureChains([mainnet], [publicProvider()])
-
-const { connectors } = getDefaultWallets({
-  appName: 'StreamETH',
-  projectId: '2f85fa1b6b2c0083df930d53eaee306f',
-  chains,
-})
-
-const config = createConfig({
-  publicClient,
-  webSocketPublicClient,
-  connectors,
-})
 
 const GeneralContext = ({ children }: { children: React.ReactNode }) => {
   const livepeerClient = useMemo(
@@ -35,12 +15,10 @@ const GeneralContext = ({ children }: { children: React.ReactNode }) => {
   )
 
   return (
-    <WagmiConfig config={config}>
+    <LivepeerConfig client={livepeerClient}>
       <Analytics />
-      <RainbowKitProvider chains={chains}>
-        <LivepeerConfig client={livepeerClient}>{children}</LivepeerConfig>
-      </RainbowKitProvider>
-    </WagmiConfig>
+      {children}
+    </LivepeerConfig>
   )
 }
 
