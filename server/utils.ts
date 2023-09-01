7588afe81b365b5607b24b7168286e0d56bd2cec
xvoorvaa <xander@xvproductions.com>
2023-09-01 17:16:02 +0200

    Added ETHChicago

--- server/utils.ts
+++ server/utils.ts
@@ -10,7 +10,7 @@ export const generateId = (key: string) => {
     .toLowerCase()
 }
 
-export const BASE_PATH = path.join(process.cwd(), 'data')
+export const BASE_PATH = path.join(process.cwd(), '../data')
 // export const BASE_PATH = "../data";
 export const PUBLIC_PATH = '../public'
 
