5538aae4806f69f63c43462ccfb20f71916d54a3
xvoorvaa <xander@xvproductions.com>
2023-09-06 12:58:16 +0000

    Fixed bug that changed all dates on the organization homepage

--- app/[organization]/[event]/(eventHome)/components/DateSelect.tsx
+++ app/[organization]/[event]/(eventHome)/components/DateSelect.tsx
@@ -2,6 +2,7 @@
 import { useContext } from 'react'
 import { ScheduleContext } from './ScheduleContext'
 import { MobileContext } from '@/components/context/MobileContext'
+
 const DateSelect = ({ dates }: { dates: number[] }) => {
   const { setDate, date } = useContext(ScheduleContext)
   const { isMobile } = useContext(MobileContext)
@@ -14,19 +15,19 @@ const DateSelect = ({ dates }: { dates: number[] }) => {
     <div className="flex flex-row space-x-4 justify-center p-2 md:p-4 box-border">
       {isMobile ? (
         <select className="text-xl cursor-pointer font-bold" value={date ? date : ''} onChange={(e) => handleDateChange(e.target.value)}>
-          {dates.map((date, index) => (
-            <option key={date} value={date}>
-              {new Date(date).toLocaleDateString()}
+          {dates.map((dateNum) => (
+            <option key={dateNum} value={dateNum}>
+              {new Date(dateNum).toLocaleDateString()}
             </option>
           ))}
         </select>
       ) : (
-        dates.map((dateString, index) => (
+        dates.map((dateNum, index) => (
           <div
-            className={` p-4 text-center text-xl font-bold ${date !== dateString ? 'text-black cursor-pointer' : 'text-accent'}`}
-            onClick={() => setDate(dateString)}
+            className={`p-4 text-center text-xl font-bold ${date !== dateNum ? 'text-black cursor-pointer' : 'text-accent'}`}
+            onClick={() => setDate(dateNum)}
             key={index}>
-            {new Date(date).toLocaleDateString()}
+            {new Date(dateNum).toLocaleDateString()}
           </div>
         ))
       )}
