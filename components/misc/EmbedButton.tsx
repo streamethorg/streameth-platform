485089eaddc20bed64b542120a4cd6ded75cb752
xvoorvaa <xander@xvproductions.com>
2023-09-06 12:37:58 +0000

    EmbedButton shows 'Copied' when pressing iframe url

--- components/misc/EmbedButton.tsx
+++ components/misc/EmbedButton.tsx
@@ -1,11 +1,17 @@
 'use client'
-import { useState, useContext } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { ModalContext } from '../context/ModalContext'
 import { CodeBracketIcon } from '@heroicons/react/24/outline'
 
-function EmbedButton({ playbackId, streamId, playerName }: { playbackId?: string; streamId?: string; playerName: string }) {
+const ModalContent: React.FC<{ playbackId?: string; streamId?: string; playerName: string }> = ({ playbackId, streamId, playerName }) => {
   const [copied, setCopied] = useState(false)
-  const { openModal } = useContext(ModalContext)
+  const copiedClass = copied ? 'opacity-100' : 'opacity-0'
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => setCopied(false), 2000)
+    }
+  }, [copied])
 
   const generateParams = () => {
     const params = new URLSearchParams()
@@ -21,9 +27,11 @@ function EmbedButton({ playbackId, streamId, playerName }: { playbackId?: string
     return `<iframe src="${url}/embed/?${generateParams()}" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`
   }
 
+  const generatedEmbedCode = generateEmbedCode()
+
   const copyToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText(generateEmbedCode())
+      await navigator.clipboard.writeText(generatedEmbedCode)
       setCopied(true)
 
       setTimeout(() => setCopied(false), 2000)
@@ -32,20 +40,29 @@ function EmbedButton({ playbackId, streamId, playerName }: { playbackId?: string
     }
   }
 
-  const handleModalOpen = () => {
-    openModal(
-      <div className="flex flex-col items-center justify-center w-full h-full px-3 py-7">
-        <div className="flex flex-col items-center justify-center w-full h-full">
-          <span className="text-md mb-4">Easily embed this stream into your website by adding the iframe code below</span>
-          <div
-            className="relative bg-gray-100 px-2 py-1 border border-gray-200 w-full max-w-full overflow-hidden whitespace-nowrap cursor-pointer"
-            onClick={copyToClipboard}>
-            {generateEmbedCode()}
-          </div>
-          {copied && <span className="absolute bottom-3 left-0 right-0 flex items-center justify-center text-accent">Copied to clipboard!</span>}
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full px-3 py-7">
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <span className="text-md mb-4">Easily embed this stream into your website by adding the iframe code below</span>
+        <div
+          className="relative bg-gray-100 px-2 py-1 border border-gray-200 w-full max-w-full overflow-hidden whitespace-nowrap cursor-pointer"
+          onClick={copyToClipboard}>
+          {generateEmbedCode()}
         </div>
+        <span
+          className={`absolute bottom-3 left-0 right-0 flex items-center justify-center text-accent transition-opacity duration-200 ${copiedClass}`}>
+          Copied to clipboard!
+        </span>
       </div>
-    )
+    </div>
+  )
+}
+
+function EmbedButton({ playbackId, streamId, playerName }: { playbackId?: string; streamId?: string; playerName: string }) {
+  const { openModal } = useContext(ModalContext)
+
+  const handleModalOpen = () => {
+    openModal(<ModalContent playbackId={playbackId} streamId={streamId} playerName={playerName} />)
   }
 
   return (
