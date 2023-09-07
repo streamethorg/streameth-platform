2b9d967e9c03bfea2a065a2ae525ef054a708676
pablov <38107177+pabloVoorvaart@users.noreply.github.com>
2023-09-07 18:26:00 +0200

    Schedule UI fix (#40)
    * fix schedule ui
    
    * prettier
    
    * remove add blank sessions
    
    * dont fetch event data
    
    * prettier

--- app/[organization]/[event]/(eventHome)/utils.ts
+++ app/[organization]/[event]/(eventHome)/utils.ts
@@ -2,12 +2,12 @@ import { ISession } from '@/server/model/session'
 import { secondsSinceMidnight } from '@/utils/time'
 
 export const getSlotRange = (session: ISession, earliestTime: number) => {
-  const start = Math.floor((secondsSinceMidnight(new Date(session.start)) - earliestTime) / 60 / 15)
+  const start = (secondsSinceMidnight(new Date(session.start)) - earliestTime) / 60 / 15
   const end = (secondsSinceMidnight(new Date(session.end)) - earliestTime) / 60 / 15
   return { start, end }
 }
 
-export const CELL_HEIGHT = 4
+export const CELL_HEIGHT = 6
 
 export const sessionsSchedulePosition = (sessions: ISession[]) => {
   if (sessions.length === 0) {
@@ -21,37 +21,4 @@ export const sessionsSchedulePosition = (sessions: ISession[]) => {
     max,
     totalSlots: Math.ceil((max - min) / 60 / 15),
   }
-}
-
-export const addBlankSessions = (sessions: ISession[], earliestTime: number): ISession[] => {
-  const blankSessions: ISession[] = []
-  let lastSession: ISession | undefined
-
-  const createBlankSession = (start: number, end: number, referenceSession: ISession): ISession => ({
-    id: `blank${referenceSession.id}`,
-    name: 'Blank',
-    start,
-    end,
-    stageId: referenceSession.stageId,
-    speakers: [],
-    description: '',
-    eventId: referenceSession.eventId,
-  })
-
-  if (sessions.length > 0 && sessions[0].start > earliestTime) {
-    const firstSession = sessions[0]
-    blankSessions.push(createBlankSession(firstSession.start - 1, firstSession.start, firstSession))
-  }
-
-  for (const session of sessions) {
-    if (lastSession) {
-      const gap = session.start - lastSession.end
-      if (gap > 0) {
-        blankSessions.push(createBlankSession(lastSession.end, session.start, lastSession))
-      }
-    }
-    lastSession = session
-  }
-
-  return [...sessions, ...blankSessions]
 }
