fdaac64d697c2dddfd53b362765154757cc3a181
pabloVoorvaart <me@pablovoorvaart.me>
2023-09-13 11:35:55 +0000

    debuging post prod

--- app/api/admin/studio/route.ts
+++ app/api/admin/studio/route.ts
@@ -2,8 +2,10 @@ import { AddOrUpdateFile, GetFile } from '@/server/utils/github'
 import Session from '@/utils/session'
 import { NextRequest, NextResponse } from 'next/server'
 import { decode } from 'js-base64'
+import SessionController from '@/server/controller/session'
 
 export const POST = async (req: NextRequest) => {
+  const sessionController = new SessionController()
   const session = await Session.fromRequest(req)
   if (!session) {
     return new NextResponse('Unauthorized', { status: 401 })
@@ -17,21 +19,19 @@ export const POST = async (req: NextRequest) => {
     return new NextResponse('Bad Request', { status: 400 })
   }
 
+  console.log(body)
+
   for (let i = 0; i < Object.keys(body.sessions).length; i++) {
     const sessionId = Object.keys(body.sessions)[i]
     const folderName = `data/sessions/${body.event.id}`
     const fileName = `${sessionId}.json`
-    const file = await GetFile(fileName, folderName)
-
-    if (file) {
-      const data = JSON.parse(decode(file.content))
-      const update = {
-        ...data,
-        source: body.sessions[sessionId],
-      }
-
-      await AddOrUpdateFile(fileName, JSON.stringify(update, null, 2), folderName)
+    const data = await sessionController.getSession(sessionId, body.event.id)
+    const update = {
+      ...data,
+      source: body.sessions[sessionId],
     }
+    console.log(update)
+    await AddOrUpdateFile(fileName, JSON.stringify(update, null, 2), folderName)
   }
 
   return new NextResponse('Ok', { status: 200 })
