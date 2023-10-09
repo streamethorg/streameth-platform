47f3d406fa34054e753737d0904d5d3c9dfe6622
xvoorvaa <xander@xvproductions.com>
2023-10-09 12:48:27 +0200

    Revert "Revert "Merge branch 'main' of github.com:streamethorg/streameth-platform""
    This reverts commit d8bb84e754e748da4b049a0ea378995a46468147.

--- app/api/organizations/[id]/events/route.ts
+++ app/api/organizations/[id]/events/route.ts
@@ -1,5 +1,8 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import EventController from '@/server/controller/event'
+import SessionController from '@/server/controller/session'
+import Session from '@/utils/session'
+import { AddOrUpdateFile } from '@/server/utils/github'
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   const eventController = new EventController()
@@ -15,18 +18,36 @@ export async function GET(request: Request, { params }: { params: { id: string }
   )
 }
 
-export async function POST(request: Request, { params }: { params: { id: string } }) {
-  const eventController = new EventController()
+export const POST = async (req: NextRequest, { params }: { params: { id: string } }) => {
   try {
+    const session = await Session.fromRequest(req)
+    if (!session) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const body = await req.json()
+
     const eventData = {
-      ...(await request.json()),
+      ...body,
       organizationId: params.id,
     }
 
-    const data = await eventController.createEvent(eventData)
-    return NextResponse.json(data)
+    const environment = process.env.NODE_ENV || 'development'
+
+    if (environment === 'development') {
+      // Create event in the fs
+      const eventController = new EventController()
+      await eventController.createEvent(eventData)
+    } else {
+      // Write data to db
+      const folderName = `data/events/${body.organizationId}`
+      const fileName = `${body.name.replace(/ /g, '_').toLowerCase()}.json`
+      await AddOrUpdateFile(fileName, JSON.stringify(body), folderName)
+    }
+
+    return NextResponse.json(eventData, { status: 200 })
   } catch (e) {
-    console.log(e)
-    return NextResponse.json({ error: 'Malformed request' }, { status: 400 })
+    console.error(e)
+    return new NextResponse('Malformed request', { status: 400 })
   }
 }
