32a50d549d343b7325d32183a3a3318ad961f730
pabloVoorvaart <me@pablovoorvaart.me>
2023-10-17 02:34:46 +0900

    ui changes

--- app/(home)/components/EventList.tsx
+++ app/(home)/components/EventList.tsx
@@ -12,7 +12,7 @@ const EventList = ({ events }: { events: IEvent[] }) => {
   }, [events])
 
   return (
-    <div className="h-full p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:overflow-scroll ">
+    <div className="h-full p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:overflow-scroll ">
       {filteredItems
         .sort((a: IEvent, b: IEvent) => {
           return b.start.getTime() - a.start.getTime()
