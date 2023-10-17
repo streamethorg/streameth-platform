ee7d6ad51c73ff5d5e66d9e2f9ab60fbcba031b8
pabloVoorvaart <me@pablovoorvaart.me>
2023-10-17 23:25:42 +0700

    hotfix: speaker path

--- app/[organization]/[event]/(eventHome)/page.tsx
+++ app/[organization]/[event]/(eventHome)/page.tsx
@@ -28,67 +28,62 @@ const EventHome = async ({ params }: { params: Params }) => {
   if (!hasData({ event })) return notFound()
 
   return (
-    <div className="flex flex-col w-full overflow-scroll h-full gap-4 ">
-      <HomePageLogoAndBanner event={event.toJson()} />
-      <div className="max-w-7xl mx-auto mt-4">
-        <div className="flex flex-col p-4">
-          <div className=" flex-col flex space-y-2 my-4 md:flex-col">
-            <h1 className="text-4xl py-4 text-center md:text-left">{event.name}</h1>
-            <div className=" flex flex-col space-y-4 text-left">
-              <p>&#128197; When: October 20th</p>
-              <p>&#9200; Time: 9:00 AM - 10:30 AM EST / 1:00 PM - 2:30 PM UTC</p>
-              <p>&#127759; Where: Live in Vietnam and Online</p>
-              <p>&#128421; Streamed @ launch.scroll.io</p>
-              <Link
-                className="bg-accent font-bold border-2 hover:text-accent  hover:bg-white  text-white border-accent px-2 py-1  rounded max-w-[8rem]"
-                href={
-                  'https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmFqZGU4dmVxcGgzanNxYjlzYjV1MXB0MGEgY19mZDE2YjdhZTIxZDA2NWI4OTUxYTU0MzM3NDQ1MTQ3MjEyYWI1OThhMjAzNzFlZjEzMjBjZWQ5ZWUzOWNhNTc0QGc&tmsrc=c_fd16b7ae21d065b8951a54337445147212ab598a20371ef1320ced9ee39ca574%40group.calendar.google.com'
-                }>
-                save the date
-              </Link>
+    <div className="flex flex-col w-full overflow-scroll h-full space-y-[5em] ">
+      <div>
+        <HomePageLogoAndBanner event={event.toJson()} />
+        <div className="max-w-7xl mx-auto mt-4">
+          <div className="flex flex-col p-4">
+            <div className=" flex-col flex space-y-2 my-4 md:flex-col">
+              <h1 className="text-4xl py-4 text-center md:text-left">{event.name}</h1>
+              <div className=" flex flex-col space-y-4 text-left">
+                <p>&#128197; When: October 20th</p>
+                <p>&#9200; Time: 9:00 AM - 10:30 AM EST / 1:00 PM - 2:30 PM UTC</p>
+                <p>&#127759; Where: Live in Vietnam and Online</p>
+                <p>&#128421; Streamed @ launch.scroll.io</p>
+                <Link
+                  className="bg-accent font-bold border-2 hover:text-accent  hover:bg-white  text-white border-accent px-2 py-1  rounded max-w-[8rem]"
+                  href={
+                    'https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmFqZGU4dmVxcGgzanNxYjlzYjV1MXB0MGEgY19mZDE2YjdhZTIxZDA2NWI4OTUxYTU0MzM3NDQ1MTQ3MjEyYWI1OThhMjAzNzFlZjEzMjBjZWQ5ZWUzOWNhNTc0QGc&tmsrc=c_fd16b7ae21d065b8951a54337445147212ab598a20371ef1320ced9ee39ca574%40group.calendar.google.com'
+                  }>
+                  save the date
+                </Link>
+              </div>
+            </div>
+            <div className="space-y-4">
+              <p>
+                Join us on Friday, October 20th, as we celebrate a monumental moment in Scroll{"'"}s journey - the launch of our Mainnet. It{"'"}s a
+                big step forward, and we want to share it with you. We{"'"}re bringing together a group of friends from The Graph, Aave/Lens, Dora,
+                and more to mark this occasion. We{"'"}ll be diving into a range of topics, but most importantly, we{"'"}ll be opening the floor for
+                an authentic conversation with community leaders from Latin America, Africa, and Southeast Asia. Together, we{"'"}ll explore how zk
+                technology is creating exciting new opportunities in these regions.
+              </p>
+              <p>
+                If you{"'"}re a developer, join us for a hands-on build session. We{"'"}re rolling up our sleeves and guiding you through the process
+                of deploying on Scroll. Here{"'"}s a sneak peek at some of the incredible minds you{"'"}ll be hearing from:
+              </p>
+              <ul className="">
+                <li className=" list-item">Bunny (Dora)</li>
+                <li>Nader (Lens)</li>
+                <li>CTRLV (Cog)</li>
+                <li>Pranav (The Graph)</li>
+                <li>Simona Pop (ENS)</li>
+              </ul>
+              <p>
+                But that{"'"}s not all. We{"'"}ve got a Founder conversation lined up. We{"'"}ll discuss what{"'"}s next post-mainnet launch, sharing
+                our vision, the challenges we{"'"}ve overcome, and the exciting journey ahead for Scroll. So, what{"'"}s on Scroll{"'"}s horizon? We
+                {"'"}ll reveal the areas we{"'"}re focusing on and how you can be a part of it. Save the Date now, and let{"'"}s celebrate together at
+                launch.scroll.io.
+              </p>
+              <p>
+                Cheers,<br></br>
+                The Scroll Team
+              </p>
             </div>
           </div>
-          <div className="space-y-4">
-            <p>
-              Join us on Friday, October 20th, as we celebrate a monumental moment in Scroll{"'"}s journey - the launch of our Mainnet. It{"'"}s a big
-              step forward, and we want to share it with you. We{"'"}re bringing together a group of friends from The Graph, Aave/Lens, Dora, and more
-              to mark this occasion. We{"'"}ll be diving into a range of topics, but most importantly, we{"'"}ll be opening the floor for an authentic
-              conversation with community leaders from Latin America, Africa, and Southeast Asia. Together, we{"'"}ll explore how zk technology is
-              creating exciting new opportunities in these regions.
-            </p>
-            <p>
-              If you{"'"}re a developer, join us for a hands-on build session. We{"'"}re rolling up our sleeves and guiding you through the process of
-              deploying on Scroll. Here{"'"}s a sneak peek at some of the incredible minds you{"'"}ll be hearing from:
-            </p>
-            <ul>
-              <li>Bunny (Dora)</li>
-              <li>Nader (Lens)</li>
-              <li>CTRLV (Cog)</li>
-              <li>Pranav (The Graph)</li>
-              <li>Simona Pop (ENS)</li>
-            </ul>
-            <p>
-              But that{"'"}s not all. We{"'"}ve got a Founder conversation lined up. We{"'"}ll discuss what{"'"}s next post-mainnet launch, sharing
-              our vision, the challenges we{"'"}ve overcome, and the exciting journey ahead for Scroll. So, what{"'"}s on Scroll{"'"}s horizon? We
-              {"'"}ll reveal the areas we{"'"}re focusing on and how you can be a part of it. Save the Date now, and let{"'"}s celebrate together at
-              launch.scroll.io.
-            </p>
-            <p>
-              Cheers,<br></br>
-              The Scroll Team
-            </p>
-          </div>
-
-          {/* <div className="flex flex-row flex-wrap justify-center items-center p-4">
-              <StageModalButton stages={stages} />
-              <Button link={`/${params.organization}/${params.event}/schedule`}>Schedule</Button>
-              <Button link={`/${params.organization}/${params.event}/speakers`}>Speakers</Button>
-              <Button link={`/${params.organization}/${params.event}/archive`}>Archive</Button>
-            </div> */}
         </div>
       </div>
       <SpeakerPageComponent params={params} />
-      <SchedulePageComponent  params={params} />
+      <SchedulePageComponent params={params} />
     </div>
   )
 }
--- app/[organization]/[event]/(eventHome)/schedule/components/SchedulePageComponent.tsx
+++ app/[organization]/[event]/(eventHome)/schedule/components/SchedulePageComponent.tsx
@@ -21,7 +21,10 @@ const SchedulePageComponent = async ({ params }: Params) => {
   const dates = getEventDays(event.start, event.end)
   return (
     <ScheduleContextProvider event={event.toJson()} stages={stages} days={dates}>
+      
       <div className="flex flex-col max-w-7xl w-full mx-auto p-2">
+      <span className=" box-border flex flex-col justify-center p-2 bg-white shadow-b w-full my-4 text-5xl">Schedule</span>
+
         <div className="text-center sticky z-10 flex flex-row flex-wrap md:flex-col bg-base justify-center">
           <DateSelect dates={dates} />
           <StageSelect stages={stages} />
