1eb0afd5a276872ae86c806eac584185498aa266
xvoorvaa <xander@xvproductions.com>
2023-10-17 21:36:18 -0600

    Realigned the Speaker Page

--- app/[organization]/[event]/(eventHome)/speakers/components/SpeakerPageComponent.tsx
+++ app/[organization]/[event]/(eventHome)/speakers/components/SpeakerPageComponent.tsx
@@ -14,8 +14,8 @@ const SpeakerPageComponent = async ({ params }: Params) => {
 
   return (
     <div className="flex flex-col max-w-7xl w-full mx-auto p-2">
-      <span className=" box-border flex flex-col justify-center p-2 bg-white shadow-b w-full my-4 text-5xl">Speakers</span>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-12">
+      <div className="w-full bg-white shadow-b my-4 p-2 text-5xl flex justify-center pb-8 font-bold">Speakers</div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-12 w-full">
         {speakers.map((speaker) => (
           <SpeakerCard key={speaker.id} speaker={speaker} path={`/${params.organization}/${params.event}/speakers/${speaker.id}`} />
         ))}
