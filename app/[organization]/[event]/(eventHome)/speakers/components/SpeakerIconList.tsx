76a2e249782d3e004a0db34be1f7d30d14f64004
xvoorvaa <xander@xvproductions.com>
2023-10-19 18:49:40 -0600

    Revert "Update SpeakerIconList.tsx"
    This reverts commit ba21f14d7ff212d254aab52fccc21d01fe98e8e8.

--- app/[organization]/[event]/(eventHome)/speakers/components/SpeakerIconList.tsx
+++ app/[organization]/[event]/(eventHome)/speakers/components/SpeakerIconList.tsx
@@ -2,16 +2,12 @@
 import Speaker, { ISpeaker } from '@/server/model/speaker'
 import SpeakerIcon from '@/app/[organization]/[event]/(eventHome)/speakers/components/SpeakerIcon'
 import SpeakerModal from './SpeakerModal'
-import { ModalContext } from '@/components/context/ModalContext'
-import { useContext } from 'react'
 
 export default function SpeakerIconList({ speakers }: { speakers: ISpeaker[] }) {
-  const { openModal } = useContext(ModalContext)
-
   return (
     <div className={`flex flex-col gap-2`}>
       {speakers.map((speaker) => (
-        <div key={speaker.id} onClick={() => openModal(<SpeakerModal sessions={null} speaker={speaker} />)} className="flex flex-row gap-2">
+        <div key={speaker.id} onClick={() => openModal(<SpeakerModal speaker={speaker} />)} className="flex flex-row gap-2">
           <SpeakerIcon size="md" key={speaker.id} speaker={speaker} />
         </div>
       ))}
