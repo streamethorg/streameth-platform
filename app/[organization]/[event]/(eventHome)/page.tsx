8718020f1bd2b00d35650b9e2cffa1a02ef846be
xvoorvaa <xander@xvproductions.com>
2023-10-19 18:49:19 -0600

    Revert "Revert "Timezone is now correct""
    This reverts commit 5a50d1a12abf7740e85d0698a1209951eba089f4.

--- app/[organization]/[event]/(eventHome)/page.tsx
+++ app/[organization]/[event]/(eventHome)/page.tsx
@@ -35,7 +35,7 @@ const EventHome = async ({ params }: { params: Params }) => {
               <h1 className="text-4xl py-4 text-center md:text-left font-bold">{event.name}</h1>
               <div className=" flex flex-col space-y-4 text-left">
                 <p>&#128197; When: October 20th</p>
-                <p>&#9200; Time: 8:00 AM - 10:30 AM EST / 12:00 AM - 2:30 PM UTC</p>
+                <p>&#9200; Time: 9:00 AM - 10:50 AM EST / 1:00 PM - 3:50 PM UTC</p>
                 <p>&#127759; Where: Live in Vietnam and Online</p>
                 <p>&#128421; Streamed @ launch.scroll.io</p>
                 <Link
