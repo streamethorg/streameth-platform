6f5b6b49d8c3111a6c2497db1f1686a2393fc0b8
pabloVoorvaart <me@pablovoorvaart.me>
2023-10-27 13:51:41 +0200

    type error

--- app/admin/[organization]/components/ImageFileUploader.tsx
+++ app/admin/[organization]/components/ImageFileUploader.tsx
@@ -13,7 +13,7 @@ interface ImageFileUploaderProps {
   toolTipHTML?: string
   aspectRatio?: number
   imageKey?: string
-  validationErrors?: Record<string, any>
+  validationErrors?: Record<string, any> | string | undefined;
   onImageSubmit: (
     event: Blob,
     key: string,
