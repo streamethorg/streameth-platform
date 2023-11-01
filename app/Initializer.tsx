d6428aafe9da5f38537c2f4f3c895e24dc8f46ed
greatsamist <ifegbenga@gmail.com>
2023-11-01 07:58:11 +0100

    fix accent color

--- app/Initializer.tsx
+++ app/Initializer.tsx
@@ -1,13 +1,19 @@
 'use client'
 import React, { ReactNode, useEffect } from 'react'
 import colors from './constants/colors'
+import { usePathname } from 'next/navigation'
 
 const Initializer = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname()
+  const isOrganization =
+    pathname === '/' || pathname.includes('/admin')
   useEffect(() => {
-    document.documentElement.style.setProperty(
-      '--colors-accent',
-      colors.accent
-    )
+    if (isOrganization) {
+      document.documentElement.style.setProperty(
+        '--colors-accent',
+        colors.accent
+      )
+    }
   }, [])
   return children
 }
