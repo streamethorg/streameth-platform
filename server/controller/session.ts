32aad17fc05130f0834d070c0c92cd8aff0e1f87
wslyvh <wslyvh@users.noreply.github.com>
2023-11-09 15:19:19 +0100

    db controller

--- server/controller/session.ts
+++ server/controller/session.ts
@@ -6,7 +6,7 @@ export default class SessionController {
   private controller: BaseController<ISession>
 
   constructor() {
-    this.controller = new BaseController<ISession>('fs')
+    this.controller = new BaseController<ISession>('db')
   }
 
   public async getSession(
