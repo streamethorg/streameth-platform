6d37ed52de4de73398fa765d0f3c26307ad16cd7
greatsamist <ifegbenga@gmail.com>
2023-11-10 07:22:15 +0100

    update index

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -38,7 +38,7 @@ export default async function Home() {
   const stageController = new StageController()
   const stage = await stageController.getStage(
     'theater',
-    'zuconnect_istanbul'
+    'zuconnect_hackathon'
   )
 
   return (
@@ -52,7 +52,7 @@ export default async function Home() {
         />
       </div>
       <div className="flex flex-col p-4 lg:overflow-hidden">
-        {/* <LiveEvent stage={stage?.toJson()} /> */}
+        <LiveEvent stage={stage?.toJson()} />
         <p className="px-4 mt-3 font-ubuntu font-bold text-blue text-xl">
           Upcoming Events
         </p>
