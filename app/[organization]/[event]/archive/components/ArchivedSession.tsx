3ef4593cdea77acf2b86628ad8f161a19b91c67d
wslyvh <wslyvh@users.noreply.github.com>
2023-11-16 21:39:13 +0300

    server components

--- app/[organization]/[event]/archive/components/ArchivedSession.tsx
+++ app/[organization]/[event]/archive/components/ArchivedSession.tsx
@@ -1,6 +1,5 @@
 import { ISession } from '@/server/model/session'
 import Image from 'next/image'
-import { useState } from 'react'
 import Link from 'next/link'
 import Card from '@/components/misc/Card'
 
@@ -13,33 +12,19 @@ const ArchivedSession = ({
   learnMore?: boolean
   goToStage?: boolean
 }) => {
-  const [image, setImage] = useState(session.coverImage)
-  const [fallback, setFallback] = useState(false)
-  const alt = '/events/' + session.eventId + '.png'
-
-  const handleError = () => {
-    // if (!fallback) {
-    //   setImage(alt)
-    //   setFallback(true)
-    // } else {
-    //   setImage('/cover.png')
-    // }
-  }
-
   const component = (
     <Card>
       <div className="aspect-video relative w-full">
         <Image
           className="rounded"
-          alt="Session image"
+          alt={session.name}
           quality={60}
-          src={image!}
+          src={session.coverImage!}
           fill
           sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
           style={{
             objectFit: 'cover',
           }}
-          onError={handleError}
         />
       </div>
       <p className=" p-2 py-4 flex flex-grow text-md ">
