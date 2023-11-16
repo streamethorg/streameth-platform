e0941e476c98a08cbd5b9e6a52aad92542b53058
greatsamist <ifegbenga@gmail.com>
2023-11-16 22:40:06 +0100

    prettier

--- app/[organization]/[event]/archive/components/FilteredItems.tsx
+++ app/[organization]/[event]/archive/components/FilteredItems.tsx
@@ -4,15 +4,17 @@ import { ISession } from '@/server/model/session'
 const FilteredItems = ({ sessions }: { sessions: ISession[] }) => {
   return (
     <div className=" w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4 md:overflow-scroll s-start">
-      {sessions.filter((session) => session.videoUrl !== undefined).map((session: ISession) => {
-        return (
-          <ArchivedSession
-            session={session}
-            key={session.id}
-            learnMore
-          />
-        )
-      })}
+      {sessions
+        .filter((session) => session.videoUrl !== undefined)
+        .map((session: ISession) => {
+          return (
+            <ArchivedSession
+              session={session}
+              key={session.id}
+              learnMore
+            />
+          )
+        })}
     </div>
   )
 }
