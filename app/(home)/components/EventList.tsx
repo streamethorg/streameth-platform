bbdfbdc7992a5ba436245bcd824ec51a4fd00f3c
greatsamist <ifegbenga@gmail.com>
2023-11-19 13:03:23 +0100

    enable video download for prog

--- app/(home)/components/EventList.tsx
+++ app/(home)/components/EventList.tsx
@@ -23,7 +23,7 @@ const EventList = ({ events }: { events: IEvent[] }) => {
   return (
     <>
       <div className="px-4 items-center flex flex-col md:flex-row mt-4 gap-5">
-        <p className="font-ubuntu font-bold text-blue text-4xl">
+        <p className="font-ubuntu font-bold text-blue text-2xl md:text-4xl">
           Past Events
         </p>
 
