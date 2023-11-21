a6a6df783d7ca39bb98c691045dc9df5eb67e4e5
xvoorvaa <xander@xvproductions.com>
2023-11-21 19:29:01 +0100

    Added an upload to Livepeer

--- packages/tools/src/scripts/join.ts
+++ packages/tools/src/scripts/join.ts
@@ -2,6 +2,10 @@ import { join } from 'path'
 import { bundle } from '@remotion/bundler'
 import { getCompositions, renderMedia } from '@remotion/renderer'
 import { RenderMediaOnProgress } from '@remotion/renderer'
+import { createClient, studioProvider } from '@livepeer/react'
+import { createReadStream } from 'fs'
+import path from 'path'
+import { webpackOverride } from '../webpack-override'
 
 let lastProgressPrinted = -1
 
@@ -14,10 +18,21 @@ const onProgress: RenderMediaOnProgress = ({ progress }) => {
   }
 }
 
+if (!process.env.LIVEPEER_APIKEY) {
+  console.error('process.env.LIVEPEER_APIKEY is not defined')
+}
+
+const { provider } = createClient({
+  provider: studioProvider({
+    apiKey: process.env.LIVEPEER_APIKEY ?? '',
+  }),
+})
+
 const start = async () => {
   console.log('Find compositions...')
   const bundled = await bundle({
     entryPoint: join(process.cwd(), 'src', 'index.ts'),
+    webpackOverride,
   })
 
   console.log('Fetching compositions...')
@@ -29,18 +44,44 @@ const start = async () => {
         continue
       }
       console.log(`Started rendering ${composition.id}`)
+
       await renderMedia({
         codec: 'h264',
         composition,
         serveUrl: bundled,
         outputLocation: `out/sessions/${composition.id}.mp4`,
-        videoBitrate: '3M',
+        videoBitrate: '5M',
         onProgress,
       })
+
       lastProgressPrinted = -1
+      await uploadAsset(`out/sessions/${composition.id}.mp4`)
     }
   }
 }
+
+async function uploadAsset(filePath: string) {
+  console.log('Uploading asset..')
+  const videoName = path.basename(filePath, '.mp4')
+  const stream = createReadStream(filePath)
+  await provider.createAsset({
+    sources: [
+      {
+        name: `${event}-${videoName}`,
+        file: stream,
+        storage: {
+          ipfs: true,
+          metadata: {
+            name: `${event}-${videoName}`,
+          },
+        },
+      },
+    ],
+  })
+
+  console.log(`Uploaded asset ${videoName}`)
+}
+
 start()
   .then(() => {
     process.exit(0)
