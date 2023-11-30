d7e08b853388f291241a26bce65295ca1949e792
greatsamist <ifegbenga@gmail.com>
2023-11-30 12:05:01 +0100

    test-lint-stage

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const buildNextEslintCommand = (filenames) =>
+  `yarn next:lint --fix --file ${filenames
+    .map((f) => path.relative(path.join("packages", "app"), f))
+    .join(" --file ")}`;
+
+const checkTypesNextCommand = () => "yarn next:check-types";
+
+module.exports = {
+  "packages/nextjs/**/*.{ts,tsx}": [
+    buildNextEslintCommand,
+    checkTypesNextCommand,
+  ],
+};
