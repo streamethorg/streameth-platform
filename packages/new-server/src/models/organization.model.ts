325af2770bccf222ffd25e8f887111d0aacaff26
Eric <ericvondee5@gmail.com>
2023-12-18 18:13:52 +0100

    organization module

---
+++ packages/new-server/src/models/organization.model.ts
@@ -0,0 +1,20 @@
+import { IOrganization } from '@interfaces/organization.interface';
+import { Schema, model } from 'mongoose';
+
+const OrganizationSchema = new Schema<IOrganization>(
+  {
+    name: { type: String, default: '', required: true, maxlength: 255 },
+    description: { type: String, default: '', required: true, maxlength: 255 },
+    url: { type: String, default: '', required: true },
+    logo: { type: String, default: '', required: true },
+    location: { type: String, default: '', required: true },
+    accentColor: { type: String, default: '' },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Organization = model<IOrganization>('Organization', OrganizationSchema);
+
+export default Organization;
