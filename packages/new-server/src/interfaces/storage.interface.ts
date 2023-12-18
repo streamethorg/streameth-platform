573b23df2d4eafbb1991d2649fa1e3793787c15f
Eric <ericvondee5@gmail.com>
2023-12-18 18:23:20 +0100

    storage controller

---
+++ packages/new-server/src/interfaces/storage.interface.ts
@@ -0,0 +1,8 @@
+export interface IStorageController<T> {
+  create: (query?: string, data?: T, path?: string) => Promise<T>;
+  update?: (id: string, data: T, path?: string) => Promise<T>;
+  findById: (id: string, path?: string) => Promise<T>;
+  findOne?: (query: {}, path?: string) => Promise<T>;
+  findAll: (query?: string, path?: string) => Promise<Array<T>>;
+  delete: (id: string, path?: string) => Promise<void>;
+}
