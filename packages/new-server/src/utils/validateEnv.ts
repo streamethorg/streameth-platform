614512c79a8601614d55e61fd9e60e2e188de7bc
Eric <ericvondee5@gmail.com>
2023-12-18 18:16:14 +0100

    env validator

--- packages/new-server/src/utils/validateEnv.ts
+++ packages/new-server/src/utils/validateEnv.ts
@@ -1,9 +1,19 @@
-import { cleanEnv, port, str } from 'envalid';
+import { bool, cleanEnv, port, str } from 'envalid';
+import { config } from 'dotenv';
+// config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config();
 
 const validateEnv = () => {
-  cleanEnv(process.env, {
+  return cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
+    DB_HOST: str(),
+    DB_PORT: str(),
+    LOG_FORMAT: str(),
+    LOG_DIR: str(),
+    ORIGIN: str(),
+    CREDENTIALS: bool(),
+    SECRET_KEY: str(),
   });
 };
 
