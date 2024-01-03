195821b5feebe352d39998ea7b393955cde9876a
Eric <ericvondee5@gmail.com>
2024-01-03 12:22:44 +0100

    event module implementation

--- packages/new-server/src/routes/index.ts
+++ packages/new-server/src/routes/index.ts
@@ -2,12 +2,14 @@
 import IndexRoute from './index.route';
 //import UsersRoute from './users.route';
 import OrganizationRoute from './organization.route';
+import EventRoute from './event.route';
 
 const Routes = [
   //new AuthRoute(),
   new IndexRoute(),
   //new UsersRoute(),
   new OrganizationRoute(),
+  new EventRoute(),
 ];
 
 export default Routes;
