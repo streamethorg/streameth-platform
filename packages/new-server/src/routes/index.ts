88ecca5a3d99d93c2b1043759b72254aea4205b7
Eric <ericvondee5@gmail.com>
2024-01-08 13:15:19 +0100

    implement speaker api route & controller

--- packages/new-server/src/routes/index.ts
+++ packages/new-server/src/routes/index.ts
@@ -5,6 +5,7 @@ import OrganizationRoute from './organization.route';
 import EventRoute from './event.route';
 import StageRoute from './stage.route';
 import SessionRoute from './session.route';
+import SpeakerRoute from './speaker.route';
 
 const Routes = [
   //new AuthRoute(),
@@ -14,6 +15,7 @@ const Routes = [
   new EventRoute(),
   new StageRoute(),
   new SessionRoute(),
+  new SpeakerRoute(),
 ];
 
 export default Routes;
