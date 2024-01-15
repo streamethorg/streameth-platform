c64795e2ab1223a36d1aa3f7e1644abcb4b989e4
Eric <ericvondee5@gmail.com>
2024-01-15 23:19:18 +0100

    refactor org module implementation

--- packages/new-server/src/models/organization.model.ts
+++ packages/new-server/src/models/organization.model.ts
@@ -1,21 +1,25 @@
-import { IOrganization } from '@interfaces/organization.interface';
+import { IOrganizationModel } from '@interfaces/organization.interface';
 import { Schema, model } from 'mongoose';
 
-const OrganizationSchema = new Schema<IOrganization>(
+const OrganizationSchema = new Schema<IOrganizationModel>(
   {
     name: { type: String, default: '', required: true, maxlength: 255 },
-    description: { type: String, default: '', required: true, maxlength: 255 },
+    description: { type: String, default: '', required: true },
     url: { type: String, default: '', required: true },
     logo: { type: String, default: '', required: true },
     location: { type: String, default: '', required: true },
     accentColor: { type: String, default: '' },
     slug: { type: String, default: '' },
+    entity: { type: String, default: '' },
   },
   {
     timestamps: true,
   },
 );
 
-const Organization = model<IOrganization>('Organization', OrganizationSchema);
+const Organization = model<IOrganizationModel>(
+  'Organization',
+  OrganizationSchema,
+);
 
 export default Organization;
