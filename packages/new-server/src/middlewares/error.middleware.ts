748df9e2b87056ea7a6b6e16f3a5ca837f0a1853
Eric <ericvondee5@gmail.com>
2024-01-16 01:29:07 +0100

    validation error handler

--- packages/new-server/src/middlewares/error.middleware.ts
+++ packages/new-server/src/middlewares/error.middleware.ts
@@ -3,22 +3,43 @@ import { NextFunction, Request, Response } from 'express';
 import { logger } from '@utils/logger';
 
 const errorMiddleware = (
-  error: HttpException,
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   try {
-    const status: number = error.status || 500;
-    const message: string = error.message || 'Something went wrong';
-
-    logger.error(
-      `[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`,
-    );
-    res.status(status).json({ message });
+    if (error.name == 'ValidateError') {
+      const err = formatErrors(error.fields);
+      return res.status(400).json({
+        message: err,
+      });
+    } else {
+      const status: number = error.status || 500;
+      const message: string = error.message || 'Something went wrong';
+      console.log('errpr', error);
+      logger.error(
+        `[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`,
+      );
+      res.status(status).json({ message });
+    }
   } catch (error) {
     next(error);
   }
 };
 
+const formatErrors = (fields: string[]) => {
+  let message = '';
+  const errors: any = Object.entries(fields)[0];
+  if (errors) {
+    const [field, details] = errors;
+    const key = field.replace(/^body\./, '');
+    if (details.value == undefined) {
+      message = `${key} is required`;
+    } else {
+      message = `${key} has ${details.message}`;
+    }
+    return message;
+  }
+};
 export default errorMiddleware;
