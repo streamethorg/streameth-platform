ac6338bf32057df089d84d7198e54199d0ae02e8
xvoorvaa <xander@xvproductions.com>
2024-01-25 18:54:22 +0100

    Removed unnecessary files

--- packages/app/app/api/search/route.ts
+++ packages/app/app/api/search/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { fetchAllSessions, fetchEvents } from '@/lib/data-back'
+import { fetchAllSessions, fetchEvents } from '@/lib/data'
 import { diceCoefficient } from 'dice-coefficient'
 
 export async function GET(request: NextRequest) {
