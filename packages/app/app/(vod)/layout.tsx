864a33864a839a19f954b0e08b1fe6638af6ed35
Xannyx <56308735+xannyxs@users.noreply.github.com>
2024-01-29 18:51:37 +0100

    Added a `<Footer>` (#357)
    * Added a `<Footer>`
    
    * Added repsonsive design
    
    * Added some margin on top

--- packages/app/app/(vod)/layout.tsx
+++ packages/app/app/(vod)/layout.tsx
@@ -1,4 +1,6 @@
 import HomePageNavbar from '@/components/Layout/HomePageNavbar'
+import Footer from '@/components/Layout/Footer'
+
 import { Suspense } from 'react'
 const Layout = async ({
   children,
@@ -6,10 +8,13 @@ const Layout = async ({
   children: React.ReactNode
 }) => {
   return (
-    <div className="w-screen h-screen max-w-screen-2xl mx-auto">
+    <div className="mx-auto w-screen max-w-screen-2xl h-screen">
       <HomePageNavbar />
       <div className=" flex flex-col p-2 lg:p-4 overflow-scroll">
         <Suspense>{children}</Suspense>
+      </div>
+      <div className="sticky mb-5 top-[100vh]">
+        <Footer />
       </div>
     </div>
   )
