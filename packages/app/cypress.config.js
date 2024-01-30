32b917da714497950ecab53f95ddcc934be041b8
xvoorvaa <xander@xvproductions.com>
2024-01-30 16:33:58 +0100

    Support file not needed

--- packages/app/cypress.config.js
+++ packages/app/cypress.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    supportFile: false,
   }
 });
