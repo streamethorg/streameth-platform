5bd914b04e3aaa4a2b5b2535e88a38d1f19ec0cc
xvoorvaa <xander@xvproductions.com>
2024-01-30 18:39:45 +0100

    Adding a few configs

--- packages/app/cypress.config.js
+++ packages/app/cypress.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {},
+  e2e: {
+    defaultCommandTimeout: 6000,
+    retries: {
+      runMode: 3,
+      openMode: 0,
+    },
+  },
 })
