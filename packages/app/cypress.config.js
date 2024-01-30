487f166ddb8af1c55838909757d95b08adda1749
Xannyx <56308735+xannyxs@users.noreply.github.com>
2024-01-30 18:47:55 +0100

    Cypress caching build and testing 4 kinds of browsers (#368)
    * Testing NextJS Cache
    
    * Added a `cache` to the testing envirment
    
    * Update test.yml
    
    * Added a `push` section
    
    * Update test.yml
    
    * Update test.yml
    
    * Adding extra that should fail if there is no cahce-key
    
    * Rewrote the file
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Hmmm please work
    
    * Update test.yml
    
    * Corrected the path
    
    * Create build_server.yml
    
    * Update test.yml
    
    * Support file not needed
    
    * Changed working directory
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Do not record vids
    
    * Added mobile testing
    
    * Update test.yml
    
    * Update test.yml
    
    * Please work
    
    * Added firefox tests
    
    * Waiting until API Call is finished
    
    * Inception now works correctly
    
    * Adding a few configs

--- packages/app/cypress.config.js
+++ packages/app/cypress.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    defaultCommandTimeout: 6000,
+    retries: {
+      runMode: 3,
+      openMode: 0,
     },
-  }
-});
+  },
+})
