3c07f3e1822e9b1e09a2e138312ea359bddfbce6
greatsamist <ifegbenga@gmail.com>
2024-02-02 20:25:46 +0100

    handle delete event

--- packages/app/lib/actions/events.ts
+++ packages/app/lib/actions/events.ts
@@ -1,15 +1,13 @@
-"use server"
-import { createEvent } from '@/lib/services/eventService'
+'use server'
+import { createEvent, deleteEvent } from '@/lib/services/eventService'
 import { cookies } from 'next/headers'
-import { IEvent } from "streameth-new-server/src/interfaces/event.interface"
-
+import { IEvent } from 'streameth-new-server/src/interfaces/event.interface'
 
 export const createEventAction = async ({
   event,
 }: {
   event: IEvent
 }) => {
-
   // create scheduelling sheet, and create folder strucutre in drive
 
   const authToken = cookies().get('user-session')?.value
@@ -17,12 +15,31 @@ export const createEventAction = async ({
     throw new Error('No user session found')
   }
   const response = await createEvent({
-    event: {...event, unlisted:true },
+    event: { ...event, unlisted: true },
     authToken,
   })
 
   if (!response) {
     throw new Error('Error creating event')
+  }
+
+  return response
+}
+
+export const deleteEventAction = async ({
+  eventSlug,
+}: {
+  eventSlug: string
+}) => {
+  const authToken = cookies().get('user-session')?.value
+
+  if (!authToken) {
+    throw new Error('No user session found')
+  }
+
+  const response = await deleteEvent({ eventSlug, authToken })
+  if (!response) {
+    throw new Error('Error deleting event')
   }
 
   return response
