3c9c93c678ff1cbf28bb7c50cb7a7a64f6177ea6
xvoorvaa <xander@xvproductions.com>
2024-02-14 14:12:37 +0100

    Fixed build issue

--- packages/new-server/src/dtos/auth/auth.dto.ts
+++ packages/new-server/src/dtos/auth/auth.dto.ts
@@ -1,51 +1,8 @@
-import { IAuth, IGoogleAuth } from '@interfaces/auth.interface';
-import { IsNotEmpty, ValidateNested, IsArray, IsString } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IAuth } from '@interfaces/auth.interface';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto implements IAuth {
   @IsNotEmpty()
   @IsString()
   token!: string;
-}
-
-class WebAuthDetails {
-  @IsNotEmpty()
-  @IsString()
-  client_id: string;
-
-  @IsNotEmpty()
-  @IsString()
-  project_id: string;
-
-  @IsNotEmpty()
-  @IsString()
-  auth_uri: string;
-
-  @IsNotEmpty()
-  @IsString()
-  token_uri: string;
-
-  @IsNotEmpty()
-  @IsString()
-  auth_provider_x509_cert_url: string;
-
-  @IsNotEmpty()
-  @IsString()
-  client_secret: string;
-
-  @IsNotEmpty()
-  @IsArray()
-  @IsString({ each: true })
-  javascript_origins: string[];
-
-  @IsNotEmpty()
-  @IsArray()
-  @IsString({ each: true })
-  redirect_uris: string[];
-}
-
-export class GoogleAuthDto implements IGoogleAuth {
-  @ValidateNested()
-  @Type(() => WebAuthDetails)
-  web: WebAuthDetails;
 }
