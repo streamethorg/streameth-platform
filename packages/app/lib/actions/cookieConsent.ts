a255f5af426f4db20f3acde9fa6f957eb819ed25
xvoorvaa <xander@xvproductions.com>
2024-02-19 18:23:07 +0100

    Changed naming of `cookie` functions

--- packages/app/lib/actions/cookieConsent.ts
+++ packages/app/lib/actions/cookieConsent.ts
@@ -5,10 +5,21 @@ import { cookies } from 'next/headers'
 const USER_CONSENT_COOKIE_KEY = 'cookie_consent'
 const EXPIRE_DATE = 12 * 31 * 24 * 60 * 60 * 1000 // 1 year
 
-export const getCookie = async (name: string) => {
+export const hasCookie = async (name: string) => {
   return cookies().has(name)
 }
 
-export const cookieConsent = async () => {
-  cookies().set(USER_CONSENT_COOKIE_KEY, 'true')
+export const getCookie = async (name: string) => {
+  return cookies().get(name)
+}
+
+/**
+ *
+ * Cookie will always be set to "Secure"
+ */
+export const setNextCookie = async (
+  cookieName: string,
+  cookieValue: string
+) => {
+  cookies().set(cookieName, cookieValue, { secure: true })
 }
