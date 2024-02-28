b104fb5b63c71cf708de6f7028e2da742b70c1b7
xvoorvaa <xander@xvproductions.com>
2024-02-28 16:05:21 +0100

    Fixed build error

--- packages/app/components/plugins/SignUp/index.tsx
+++ packages/app/components/plugins/SignUp/index.tsx
@@ -17,7 +17,7 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { toast } from 'sonner'
 import { ReloadIcon } from '@radix-ui/react-icons'
-import InfoHoverCard from '@/components/misc/InfoHoverCard'
+import InfoHoverCard from '@/components/misc/interact/InfoHoverCard'
 
 const initialState = {
   message: '',
