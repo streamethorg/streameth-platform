01746a5add65eee73c1d9eaf8961389564915197
nightblood <ericvondee5@gmail.com>
2024-03-07 11:56:19 +0100

    contract-setup

---
+++ packages/contracts/scripts/deploy.ts
@@ -0,0 +1,11 @@
+import { ethers } from "hardhat";
+
+async function main() {
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
