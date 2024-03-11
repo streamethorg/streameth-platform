3ea88e37c8a62d2af0c4300ae37ecaaad3b74f80
xvoorvaa <xander@xvproductions.com>
2024-03-11 18:15:55 +0100

    Added navigation and footer tests

--- packages/app/cypress.config.js
+++ packages/app/cypress.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on, config) { },
+    baseUrl: 'http://localhost:3000',
     defaultCommandTimeout: 6000,
     retries: {
       runMode: 3,
---
+++ packages/app/cypress/e2e/navigation.cy.js
@@ -0,0 +1,19 @@
+describe('Navigation', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/')
+  })
+
+  it('should navigate to the info page', () => {
+    cy.get(
+      'a[href*="https://info.streameth.org/stream-eth-studio"]'
+    ).click()
+  })
+
+  it('Click on the logo to redirect to the homepage again', () => {
+    cy.get('nav > a[href*="/"]').click('topLeft')
+  })
+})
