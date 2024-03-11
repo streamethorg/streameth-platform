3b70a754849d6310f94e60bddeb104f2b22196e5
xvoorvaa <xander@xvproductions.com>
2024-03-11 19:20:43 +0100

    Added a few more extra tests

--- packages/app/cypress/e2e/home.cy.js
+++ packages/app/cypress/e2e/home.cy.js
@@ -4,12 +4,13 @@ describe('Tests for homepage', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:3000/')
-    cy.scrollTo('bottom')
+    cy.visit('/')
   })
+
   Cypress.on('uncaught:exception', (err, runnable) => {
     return false
   })
+
   it('Displays a list of organizations, which should have text', () => {
     cy.get(':nth-child(3) > .my-2 > a > .font-semibold').should(
       'not.be.empty'
@@ -41,5 +42,15 @@ describe('Tests for homepage', () => {
 
   it('can click organization name', () => {
     cy.get(':nth-child(1) > .my-2 > a > .font-semibold').click()
+  })
+
+  it('Video is playing on the homepage', () => {
+    cy.get('div > video')
+      .should('exist')
+      .then(($video) => {
+        const videoElement = $video.get(0)
+
+        expect(videoElement.paused).to.be.false
+      })
   })
 })
--- packages/app/cypress/e2e/navigation.cy.js
+++ packages/app/cypress/e2e/navigation.cy.js
@@ -16,4 +16,12 @@ describe('Navigation', () => {
   it('Click on the logo to redirect to the homepage again', () => {
     cy.get('nav > a[href*="/"]').click('topLeft')
   })
+
+  it('Shows a 404 page for non-existent routes', () => {
+    cy.visit('/some-non-existent-route', { failOnStatusCode: false })
+
+    cy.get('h2.text-4xl.font-bold.mb-4')
+      .contains('404: Not Found')
+      .should('exist')
+  })
 })
