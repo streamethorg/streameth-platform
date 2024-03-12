ce0870f180e6ae250fd77b3a0fc5e8b10b3295bd
xvoorvaa <xander@xvproductions.com>
2024-03-12 22:26:16 +0100

    Update cypress.config.js

--- packages/app/cypress.config.js
+++ packages/app/cypress.config.js
@@ -4,7 +4,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) { },
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 6000,
+    defaultCommandTimeout: 12000,
+    pageLoadTimeout: 60000,
     retries: {
       runMode: 3,
       openMode: 0,
