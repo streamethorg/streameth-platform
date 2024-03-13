268d0e9a3a86be19afdc1f1acb16fa0e61fb7ef7
xvoorvaa <xander@xvproductions.com>
2024-03-13 15:14:52 +0100

    Update cookie.cy.js

--- packages/app/cypress/e2e/cookie.cy.js
+++ packages/app/cypress/e2e/cookie.cy.js
@@ -1,32 +1,24 @@
 describe('Cookie Banner', () => {
   beforeEach(() => {
-    cy.clearCookies()
     cy.visit('/')
+    cy.clearCookies()
+
+    cy.get('.fixed > .relative', { timeout: 10000 })
+      .should('exist')
+      .as('cookieBanner')
   })
 
   it('See if Cookie Banner exist', () => {
-    cy.get('div[role="alert"]', { timeout: 10000 })
+    cy.get('@cookieBanner')
       .should('contain.text', 'We Use Cookies 🍪')
       .and('be.visible')
   })
 
-  it('Accepts cookies and checks for the cookie_consent cookie', () => {
-    cy.get('div[role="alert"]', { timeout: 10000 })
-      .contains('button', 'Ok')
-      .click()
-
-    cy.getCookie('cookie_consent').then((cookie) => {
-      expect(cookie.value).to.eq('true')
-      expect(cookie.secure).to.be.true
-    })
-  })
-
   it('Accepts cookies on archive page and checks for the cookie_consent cookie', () => {
     cy.visit('/archive')
-    cy.get('div[role="alert"]', { timeout: 10000 })
-      .contains('button', 'Ok')
-      .click()
+    cy.get('@cookieBanner').get('.mt-3 > .mr-1').click()
 
+    cy.wait(4000) // Wait for cookie to process
     cy.getCookie('cookie_consent').then((cookie) => {
       expect(cookie.value).to.eq('true')
       expect(cookie.secure).to.be.true
@@ -34,9 +26,7 @@ describe('Cookie Banner', () => {
   })
 
   it('"More information..." should redirect to "/privacy"', () => {
-    cy.get('div[role="alert"] a[href*="/privacy"]', {
-      timeout: 10000,
-    }).click()
+    cy.get('@cookieBanner').find('a[href*="/privacy"]').click()
     cy.url().should('include', '/privacy')
   })
 })
