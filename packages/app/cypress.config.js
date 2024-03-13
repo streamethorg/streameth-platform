73750cdf70f86910a08f0bd3c784b55fc7fd7554
xvoorvaa <xander@xvproductions.com>
2024-03-13 15:49:45 +0100

    Changes to the CI/CD

--- packages/app/cypress.config.js
+++ packages/app/cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) { },
+    projectId: 't4kovj',
     baseUrl: 'http://localhost:3000',
     defaultCommandTimeout: 12000,
     pageLoadTimeout: 60000,
