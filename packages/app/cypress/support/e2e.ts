6c5308391f0b3c5767904124c34e23bd272da230
xvoorvaa <xander@xvproductions.com>
2024-03-13 15:34:57 +0100

    Update e2e.ts

--- packages/app/cypress/support/e2e.ts
+++ packages/app/cypress/support/e2e.ts
@@ -17,19 +17,19 @@
 import './commands'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  const errorsToIgnore = [
-    'NEXT_NOT_FOUND',
-    'Hydration failed because the initial UI does not match what was rendered on the server',
-    'There was an error while hydrating this Suspense boundary. Switched to client rendering.',
-  ]
+  // const errorsToIgnore = [
+  //   'NEXT_NOT_FOUND',
+  //   'Hydration failed because the initial UI does not match what was rendered on the server',
+  //   'There was an error while hydrating this Suspense boundary. Switched to client rendering.',
+  // ]
+  //
+  // const shouldIgnoreError = errorsToIgnore.some((errorText) =>
+  //   err.message.includes(errorText)
+  // )
 
-  const shouldIgnoreError = errorsToIgnore.some((errorText) =>
-    err.message.includes(errorText)
-  )
-
-  if (shouldIgnoreError) {
-    return false
-  }
+  // if (shouldIgnoreError) {
+  return false
+  // }
 
   return undefined
 })
