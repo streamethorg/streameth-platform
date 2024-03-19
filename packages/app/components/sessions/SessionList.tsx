ba926333c7e7f13e6022a2d37074a80249aefbbf
greatsamist <ifegbenga@gmail.com>
2024-03-19 13:59:18 +0100

    Revert "event bug fixes"
    This reverts commit 3e8eeaaf3cbed45fa504eb9851a6fa433b7ccd16.

--- packages/app/components/sessions/SessionList.tsx
+++ packages/app/components/sessions/SessionList.tsx
@@ -36,26 +36,25 @@ export default function SessionList({
     )
   }
 
-  const sortedSessions =
-    getCurrDaySessions().length && !date
-      ? getCurrDaySessions().sort((a, b) => {
-          if (a.start < b.start) {
-            return -1
-          } else if (a.start > b.start) {
-            return 1
-          } else {
-            return 0
-          }
-        })
-      : sessions.slice().sort((a, b) => {
-          if (a.start < b.start) {
-            return -1
-          } else if (a.start > b.start) {
-            return 1
-          } else {
-            return 0
-          }
-        })
+  const sortedSessions = getCurrDaySessions().length
+    ? getCurrDaySessions().sort((a, b) => {
+        if (a.start < b.start) {
+          return -1
+        } else if (a.start > b.start) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+    : sessions.slice().sort((a, b) => {
+        if (a.start < b.start) {
+          return -1
+        } else if (a.start > b.start) {
+          return 1
+        } else {
+          return 0
+        }
+      })
 
   useEffect(() => {
     if (currentSession) {
