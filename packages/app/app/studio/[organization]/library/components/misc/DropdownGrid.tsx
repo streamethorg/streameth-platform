2f351a7d132708f0b1b4897694b480d890071bc6
Xannyx <56308735+xannyxs@users.noreply.github.com>
2024-04-05 19:05:03 +0200

    Asset page bug fixes (#505)
    * Updated yarn
    
    * Refactored Asset Page
    
    * Prettier
    
    * Added upload Dialog
    
    * Removed old upload method
    
    * Created EditSession
    
    * Added new variables to session
    
    * Reverted some changed from Pablo
    
    * Moved files
    
    * Moved uploading logic to the back-end
    
    * Added button functionality
    
    * Added upload logic
    
    * Ordered layouts
    
    * Upgraded Lucide
    
    * Update yarn.lock
    
    * Added Grid layout
    
    * Fixed minor bugs
    
    * Moved files and a bit of responsive design
    
    * Fixed build errror
    
    * Made StageId optional
    
    * Minor changes to types
    
    * Fixed build
    
    * Added new SVGs and completed a few tasks
    
    * Fixed hydration issue
    
    * Added 'created at' sort
    
    * Fixed build
    
    * Changed Grid to fix hydration
    
    * Fixed build issues
    
    * Video downloadable
    
    * Minor bug to delete asset
    
    * Fixed progress bar
    
    * Fixed hydration issue
    
    * Fixed minor bugs

--- packages/app/app/studio/[organization]/library/components/misc/DropdownGrid.tsx
+++ packages/app/app/studio/[organization]/library/components/misc/DropdownGrid.tsx
@@ -13,6 +13,7 @@ import { Button } from '@/components/ui/button'
 import DeleteAsset from '../DeleteAsset'
 import { IExtendedSession } from '@/lib/types'
 import Link from 'next/link'
+import { toast } from 'sonner'
 
 const TriggerComponent = (): ReactNode => {
   return (
@@ -34,17 +35,19 @@ export const DropdownItems = ({
 }): ReactNode => {
   const handleCopy = () => {
     navigator.clipboard.writeText(session.ipfsURI as string)
+    toast.success('Copied IPFS Hash to your clipboard')
   }
 
   return (
     <div className="flex flex-col justify-start">
       <DropdownMenuItem>
         <Link
-          className="flex flex-row justify-center items-center space-x-2"
           href={`/studio/${organizationSlug}/library/${
             session._id as string
           }`}>
-          <Button variant={'ghost'}>
+          <Button
+            variant={'ghost'}
+            className="flex flex-row justify-center items-center space-x-2">
             <FilePenLine className="text-muted-foreground" />
             <p className="">Edit</p>
           </Button>
