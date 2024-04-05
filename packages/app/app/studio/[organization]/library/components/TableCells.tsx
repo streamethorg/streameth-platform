43ac2e2a6c6d9596133af76c2ac00e18616209da
xvoorvaa <xander@xvproductions.com>
2024-04-05 19:29:13 +0200

    User able to publish a video

--- packages/app/app/studio/[organization]/library/components/TableCells.tsx
+++ packages/app/app/studio/[organization]/library/components/TableCells.tsx
@@ -2,7 +2,7 @@
 
 import { TableCell } from '@/components/ui/table'
 import { IExtendedSession, eLayout } from '@/lib/types'
-import { Copy } from 'lucide-react'
+import { ChevronDown, Copy, Earth, Lock } from 'lucide-react'
 import PopoverActions from './PopoverActions'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -10,6 +10,13 @@ import DefaultThumbnail from '@/lib/svg/DefaultThumbnail'
 import { AspectRatio } from '@radix-ui/react-aspect-ratio'
 import { formatDate } from '@/lib/utils/time'
 import { toast } from 'sonner'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { updateSessionAction } from '@/lib/actions/sessions'
 
 const TableCells = ({
   item,
@@ -21,6 +28,35 @@ const TableCells = ({
   const handleCopy = () => {
     navigator.clipboard.writeText(item.ipfsURI!)
     toast.success('Copied IPFS Hash to your clipboard')
+  }
+
+  const handlePublishment = () => {
+    updateSessionAction({
+      session: {
+        _id: item._id,
+        name: item.name,
+        description: item.description,
+        organizationId: item.organizationId,
+        eventId: item.eventId,
+        stageId: item.stageId,
+        start: item.start ?? Number(new Date()),
+        end: item.end ?? Number(new Date()),
+        speakers: item.speakers ?? [],
+        type: item.type,
+        published: !item.published,
+      },
+    })
+      .then(() => {
+        if (item.published === true) {
+          toast.success('Succesfully made your asset private')
+        } else if (item.published === false) {
+          toast.success('Succesfully made your asset public')
+        }
+      })
+      .catch(() => {
+        toast.error('Something went wrong...')
+      })
+      .finally(() => location.reload())
   }
 
   return (
@@ -51,7 +87,43 @@ const TableCells = ({
           </Link>
         </div>
       </TableCell>
-      <TableCell>{item.published ? 'Public' : 'Private'}</TableCell>
+      <TableCell>
+        <div className="flex justify-start items-center space-x-2">
+          {item.published ? (
+            <>
+              <Earth size={16} />
+              <p>Public</p>
+            </>
+          ) : (
+            <>
+              <Lock size={16} />
+              <p>Private</p>
+            </>
+          )}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <ChevronDown size={20} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                className="space-x-2 cursor-pointer"
+                onClick={() => handlePublishment()}>
+                {!item.published ? (
+                  <>
+                    <Earth size={16} />
+                    <p>Make Public</p>
+                  </>
+                ) : (
+                  <>
+                    <Lock size={16} />
+                    <p>Make Private</p>
+                  </>
+                )}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </TableCell>
       {item.createdAt && (
         <TableCell className="truncate">
           {formatDate(
