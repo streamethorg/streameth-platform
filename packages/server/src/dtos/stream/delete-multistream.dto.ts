fc3d5dc24e57d2ba31b4d8a4ac7f58de586deb25
nightblood <ericvondee5@gmail.com>
2024-04-10 23:13:55 +0100

    create & delete multistream integration

---
+++ packages/server/src/dtos/stream/delete-multistream.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class DeleteMultiStreamDto {
+  @IsNotEmpty()
+  @IsString()
+  streamId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  targetId: string;
+}
