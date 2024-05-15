f35e108a374ac13a3534e6fdc92711512ba4f3db
greatsamist <ifegbenga@gmail.com>
2024-05-15 13:34:45 +0100

    privy-fe-auth

--- packages/app/components/authorization/AuthorizationMessage.tsx
+++ packages/app/components/authorization/AuthorizationMessage.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import { ConnectWalletButton } from '../misc/ConnectWalletButton'
 import {
   Card,
@@ -7,32 +8,36 @@ import {
   CardTitle,
   CardContent,
 } from '@/components/ui/card'
-import SignInWithSocials from './SignInWithSocials'
+
 import LoginBackground from '@/public/login-background.png'
 import Image from 'next/image'
+
+import { usePrivy } from '@privy-io/react-auth'
+import { toast } from 'sonner'
+
 const AuthorizationMessage = () => {
+  const { ready, authenticated, login } = usePrivy()
+
+  useEffect(() => {
+    if (ready && !authenticated) {
+      login()
+    }
+    if (authenticated) {
+      toast.message('Redirecting to Studio')
+    }
+  }, [ready, authenticated])
+
   return (
     <div className="flex flex-row h-screen w-screen">
       <div className="w-1/2 h-full flex flex-col items-center justify-center">
         <Card className="max-w-[500px] shadow-none ">
           <CardHeader className="text-center">
             <CardTitle>Welcome to StreamETH</CardTitle>
-            <CardDescription>
-              Connect your wallet to continue.
-            </CardDescription>
+            <CardDescription>Log-in / Sign-up</CardDescription>
             <div className="w-full pt-[20px] flex items-center justify-center">
               <ConnectWalletButton />
             </div>
           </CardHeader>
-
-          <div className="my-4 px-4 flex flex-row justify-between items-center space-x-2">
-            <p className="w-full border" />
-            <div>OR</div>
-            <p className="w-full border" />
-          </div>
-          <CardContent>
-            <SignInWithSocials />
-          </CardContent>
         </Card>
       </div>
       <div className="w-1/2 h-full bg-primary relative">
@@ -43,11 +48,6 @@ const AuthorizationMessage = () => {
           layout="fill"
           objectFit="cover"
         />
-        {/* <img
-          src="/login-background.png"
-          alt="login background"
-          className="w-full h-full object-cover"
-        /> */}
       </div>
       <div></div>
     </div>
