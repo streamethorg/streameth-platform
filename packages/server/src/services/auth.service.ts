d7afe0a52bc6503d913ff3f4e8424199224ceecd
Eric-Vondee <ericvondee5@gmail.com>
2024-05-22 07:59:56 +0100

    verify token

--- packages/server/src/services/auth.service.ts
+++ packages/server/src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpException } from '@exceptions/HttpException';
 import jwt from 'jsonwebtoken';
 import { config } from '@config';
 import UserService from './user.service';
-import { PrivyClient } from '@privy-io/server-auth';
+import privy from '@utils/privy';
 
 export default class AuthService {
   private userService = new UserService();
@@ -12,11 +12,9 @@ export default class AuthService {
     this.path = 'auth';
   }
 
-  private privy = new PrivyClient(config.privy.appId, config.privy.appSecret);
-
   async login(data: IUser): Promise<{ user: IUser; token: string }> {
     let verifyToken = await this.verifyAuthToken(data.token);
-    let user = await this.privy.getUser(verifyToken.userId);
+    let user = await privy.getUser(verifyToken.userId);
     let existingUser = await this.userService.findOne({
       walletAddress: user.wallet.address,
     });
@@ -40,7 +38,7 @@ export default class AuthService {
     token: string,
   ): Promise<{ userId: string; sessionId: string }> {
     try {
-      const authToken = await this.privy.verifyAuthToken(token);
+      const authToken = await privy.verifyAuthToken(token);
       return {
         userId: authToken.userId,
         sessionId: authToken.sessionId,
@@ -49,6 +47,21 @@ export default class AuthService {
       if (e.code == 'ERR_JWT_EXPIRED') {
         throw new HttpException(401, 'Jwt Expired');
       }
+    }
+  }
+
+  async verifyToken(token: string): Promise<boolean> {
+    try {
+      jwt.verify(token, config.jwt.secret);
+      return true;
+    } catch (e) {
+      if (
+        e instanceof jwt.TokenExpiredError ||
+        e instanceof jwt.JsonWebTokenError
+      ) {
+        return false;
+      }
+      return false;
     }
   }
 }
