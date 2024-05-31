7cad620b7c00d0a143bab02e53bd76a57ec86d0d
greatsamist <ifegbenga@gmail.com>
2024-05-31 17:20:44 +0100

    Update Connect Wallet Button

--- packages/app/components/misc/ConnectWalletButton.tsx
+++ packages/app/components/misc/ConnectWalletButton.tsx
@@ -1,78 +1,29 @@
 'use client'
+import { ConnectKitButton } from 'connectkit'
 import { Button } from '@/components/ui/button'
-import { useLogin, useLogout, usePrivy } from '@privy-io/react-auth'
-import { deleteSession, storeSession } from '@/lib/actions/auth'
-import { apiUrl } from '@/lib/utils/utils'
-import { Loader2 } from 'lucide-react'
-
 interface ConnectWalletButtonProps {
   className?: string
   btnText?: string
 }
 
 export const ConnectWalletButton = ({
-  btnText = 'Sign in',
+  btnText = 'Connect Wallet',
   className,
 }: ConnectWalletButtonProps) => {
-  const { ready, authenticated } = usePrivy()
-
-  const getSession = async () => {
-    const privyToken = localStorage.getItem('privy:token')
-    const token = privyToken ? JSON.parse(privyToken) : null
-    const res = await fetch(`${apiUrl()}/auth/login`, {
-      method: 'POST',
-      body: JSON.stringify({
-        token: token,
-      }),
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    })
-    const resData = await res.json()
-    storeSession({
-      token: resData?.data?.token,
-      address: resData?.data?.user?.walletAddress,
-    })
-  }
-
-  const { login } = useLogin({
-    onComplete: () => {
-      getSession()
-    },
-    onError: (error) => {
-      deleteSession()
-    },
-  })
-
-  const { logout } = useLogout({
-    onSuccess: () => {
-      deleteSession()
-    },
-  })
-
   return (
-    <Button
-      onClick={authenticated ? logout : login}
-      className={className}>
-      {!ready ? (
-        <Loader2 className="mr-2 w-4 h-4 animate-spin" />
-      ) : authenticated ? (
-        'Sign Out'
-      ) : (
-        btnText
-      )}
-    </Button>
+    <ConnectKitButton.Custom>
+      {({ isConnected, show, truncatedAddress, ensName }) => {
+        return (
+          <Button
+            variant={'primary'}
+            onClick={show}
+            className={className}>
+            <span className="px-2 md:px-0">
+              {isConnected ? ensName ?? truncatedAddress : btnText}
+            </span>
+          </Button>
+        )
+      }}
+    </ConnectKitButton.Custom>
   )
 }
-;[
-  {
-    guid: 'c0105a4c-9b81-558a-b4a2-b193c27d482f',
-    id: 307,
-    code: 'ZQWHEL',
-    public_name: 'Stanley Qiufan CHEN',
-    avatar:
-      'https://cfp.ducttape.events/media/avatars/WechatIMG44_mGHjpPV.jpeg',
-    biography:
-      'Chen Qiufan (a.k.a. Stanley Chan) is an award-winning speculative fiction author, creative producer, research fellow, and early investor. His works include Waste Tide and AI 2041: Ten Visions for Our Future (co-authored with Dr. Kai-Fu Lee).',
-    answers: [],
-  },
-]
