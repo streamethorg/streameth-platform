49b3cc744b34291f97cc0c5f54f40ac02ad7a464
Eric-Vondee <ericvondee5@gmail.com>
2024-06-28 13:38:01 +0100

    rabbitmq (#692)

--- packages/server/src/utils/rabbitmq.ts
+++ packages/server/src/utils/rabbitmq.ts
@@ -1,53 +1,53 @@
-// import { config } from '@config';
-// import amqp from 'amqplib';
-// import { logger } from './logger';
-// const { host, port, username, secret } = config.mq;
+import { config } from '@config';
+import amqp from 'amqplib';
+import { logger } from './logger';
+const { host, port, username, secret } = config.mq;
 
-// const RETRY_INTERVAL = 5000; // 5 seconds
-// const MAX_RETRIES = 5;
-// async function connectToRabbitMQ(retries = 0) {
-//   try {
-//     const connection = await amqp.connect({
-//       protocol: 'amqp',
-//       hostname: host,
-//       port: port,
-//       username: username,
-//       password: secret,
-//       vhost: '/',
-//     });
-//     logger.info('RabbitMQ connected', connection);
+const RETRY_INTERVAL = 5000; // 5 seconds
+const MAX_RETRIES = 5;
+async function connectToRabbitMQ(retries = 0) {
+  try {
+    const connection = await amqp.connect({
+      protocol: 'amqp',
+      hostname: host,
+      port: port,
+      username: username,
+      password: secret,
+      vhost: '/',
+    });
+    logger.info('RabbitMQ connected', connection);
 
-//     connection.on('error', (e) => {
-//       logger.error('RabbitMQ connection error:', e);
-//       if (e.message !== 'Connection closing') {
-//         reconnectToRabbitMQ();
-//       }
-//     });
+    connection.on('error', (e) => {
+      logger.error('RabbitMQ connection error:', e);
+      if (e.message !== 'Connection closing') {
+        reconnectToRabbitMQ();
+      }
+    });
 
-//     connection.on('close', () => {
-//       logger.warn('RabbitMQ connection closed');
-//       reconnectToRabbitMQ();
-//     });
+    connection.on('close', () => {
+      logger.warn('RabbitMQ connection closed');
+      reconnectToRabbitMQ();
+    });
 
-//     return connection;
-//   } catch (e) {
-//     if (retries < MAX_RETRIES) {
-//       logger.warn(
-//         `RabbitMQ connection failed. Retrying in ${RETRY_INTERVAL / 1000} seconds...`,
-//       );
-//       setTimeout(() => connectToRabbitMQ(retries + 1), RETRY_INTERVAL);
-//     } else {
-//       logger.error('Max retries reached. Could not connect to RabbitMQ:', e);
-//       return;
-//     }
-//   }
-// }
+    return connection;
+  } catch (e) {
+    if (retries < MAX_RETRIES) {
+      logger.warn(
+        `RabbitMQ connection failed. Retrying in ${RETRY_INTERVAL / 1000} seconds...`,
+      );
+      setTimeout(() => connectToRabbitMQ(retries + 1), RETRY_INTERVAL);
+    } else {
+      logger.error('Max retries reached. Could not connect to RabbitMQ:', e);
+      return;
+    }
+  }
+}
 
-// function reconnectToRabbitMQ() {
-//   logger.info(
-//     `Reconnecting to RabbitMQ in ${RETRY_INTERVAL / 1000} seconds...`,
-//   );
-//   setTimeout(() => connectToRabbitMQ(), RETRY_INTERVAL);
-// }
-// const connection = connectToRabbitMQ();
-// export default connection;
+function reconnectToRabbitMQ() {
+  logger.info(
+    `Reconnecting to RabbitMQ in ${RETRY_INTERVAL / 1000} seconds...`,
+  );
+  setTimeout(() => connectToRabbitMQ(), RETRY_INTERVAL);
+}
+const connection = connectToRabbitMQ();
+export default connection;
