f4ee2cef824af5b758141da1ed50b9ff9bb8a963
pablov <38107177+pblvrt@users.noreply.github.com>
2024-07-03 10:31:27 +0200

    🐛 Fix Sidebar z index and dark label #698 (#711)
    Fix homepage button hover state, logo and font
    Closes #698
    Fix dark text label on sidebar

--- packages/app/components/Sidebar/SidebarMenu.tsx
+++ packages/app/components/Sidebar/SidebarMenu.tsx
@@ -27,11 +27,11 @@ const SidebarMenu = ({
       navigationPath: '/library',
       icon: <LuVideotape size={25} />,
     },
-    {
-      text: 'Destinations',
-      navigationPath: '/destinations',
-      icon: <LuShare2 size={25} />,
-    },
+    // {
+    //   text: 'Destinations',
+    //   navigationPath: '/destinations',
+    //   icon: <LuShare2 size={25} />,
+    // },
     {
       text: 'NFTs',
       navigationPath: '/nfts',
@@ -42,6 +42,11 @@ const SidebarMenu = ({
       navigationPath: '/team',
       icon: <LuUsers size={25} />,
     },
+    // {
+    //   text: 'Collections',
+    //   navigationPath: '/nfts',
+    //   icon: <LuImage size={25} />,
+    // },
     {
       text: 'Settings',
       navigationPath: `/settings`,
