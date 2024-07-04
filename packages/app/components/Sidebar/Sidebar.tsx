99308cb2f9816f7fb2fb139994c73523e294ffda
mariodev <gh_mario.proclaim@slmail.me>
2024-07-04 05:39:40 -0400

    🐛 Fixed #719 plus moved the docs icon with the rest

--- packages/app/components/Sidebar/Sidebar.tsx
+++ packages/app/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { ChevronLast, ChevronFirst, BookOpenText } from 'lucide-react'
-import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import { useContext, createContext, useState, ReactNode } from 'react'
 import Logo from '@/public/logo.png'
 import LogoDark from '@/public/logo_dark.png'
 import Image from 'next/image'
+
 const SidebarContext = createContext<
   { expanded: boolean } | undefined
 >(undefined)
@@ -23,9 +23,8 @@ export const SidebarUI = ({
     <aside className="flex flex-col h-screen bg-white border-r">
       <div className="flex justify-between items-center p-4">
         <div
-          className={`flex items-center overflow-hidden transition-all ${
-            expanded ? 'w-40' : 'w-10'
-          }`}>
+          className={`flex items-center overflow-hidden transition-all ${expanded ? 'w-40' : 'w-10'
+            }`}>
           <div className="transition-all">
             {expanded ? (
               <Image
@@ -42,11 +41,13 @@ export const SidebarUI = ({
       </div>
 
       <SidebarContext.Provider value={{ expanded }}>
-        <ul className="flex-1 px-3 space-y-4">{children}</ul>
+        <ul className="flex-1 px-3 space-y-4">
+          {children}
+        </ul>
       </SidebarContext.Provider>
       <button
         onClick={() => setExpanded((curr) => !curr)}
-        className="p-1.5 text-black mx-auto">
+        className="p-1.5 text-black mx-auto mb-4">
         {expanded ? (
           <span className="flex flex-row w-full">
             <ChevronFirst />
@@ -56,30 +57,6 @@ export const SidebarUI = ({
           <ChevronLast />
         )}
       </button>
-      <Link
-        className="flex relative items-center justify-center space-x-2 p-2 mx-auto w-full mb-8 rounded-lg transition-colors text-black group"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://streameth.notion.site/StreamETH-Docs-f31d759cea824b0ea8f959a4608b0b42">
-        <BookOpenText className="w-6 h-6" />
-        <span
-          className={`overflow-hidden transition-all ${
-            expanded ? '' : 'w-0'
-          }`}>
-          Docs
-        </span>
-        {!expanded && (
-          <div
-            className={`
-          absolute left-full rounded-md px-2 py-1 ml-2
-          bg-primary text-sm text-white
-          invisible opacity-20 -translate-x-3 transition-all
-          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
-      `}>
-            Docs
-          </div>
-        )}
-      </Link>
     </aside>
   )
 }
@@ -88,10 +65,12 @@ export const SidebarItem = ({
   icon,
   text,
   navigationPath,
+  isExternal = false
 }: {
   icon: ReactNode
   text: string
   navigationPath: string
+  isExternal?: boolean
 }) => {
   const router = useRouter()
   const pathname = usePathname()
@@ -100,41 +79,42 @@ export const SidebarItem = ({
     expanded: true,
   }
 
-  const handleRoute = () => {
-    router.push(navigationPath)
+  const handleClick = () => {
+    if (isExternal) {
+      window.open(navigationPath, '_blank', 'noopener,noreferrer')
+    } else {
+      router.push(navigationPath)
+    }
   }
 
   return (
     <li
-      onClick={handleRoute}
+      onClick={handleClick}
       className={`
         relative flex items-center py-2
-            font-medium rounded-md cursor-pointer
-            transition-colors group   hover:border hover:border-primary hover:rounded-xl
-            ${
-              active
-                ? 'border border-primary rounded-xl '
-                : 'border border-white '
-            } 
-            ${expanded ? 'px-2' : 'justify-center '}
-    `}>
+        font-medium rounded-md cursor-pointer
+        transition-colors group hover:border hover:border-primary hover:rounded-xl
+        ${active && !isExternal ? 'border border-primary rounded-xl' : 'border border-white'}
+        ${expanded ? 'px-2' : 'justify-center'}
+      `}
+    >
       {icon}
       <span
-        className={`overflow-hidden transition-all text-sm ${
-          expanded ? ' ml-3' : 'hidden'
-        }`}>
+        className={`overflow-hidden transition-all text-sm ${expanded ? 'ml-3' : 'hidden'}`}
+      >
         {text}
       </span>
 
       {!expanded && (
         <div
           className={`
-          absolute left-full rounded-md px-2 py-1 ml-4
-          z-[9999]
-          bg-primary text-sm text-white
-          invisible opacity-20 -translate-x-3 transition-all
-          group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
-      `}>
+            absolute left-full rounded-md px-2 py-1 ml-4
+            z-[9999]
+            bg-primary text-sm text-white
+            invisible opacity-20 -translate-x-3 transition-all
+            group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+          `}
+        >
           {text}
         </div>
       )}
