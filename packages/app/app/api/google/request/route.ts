40ec1fd89bb0e8d5054c088819a39d5e1f6e067b
greatsamist <ifegbenga@gmail.com>
2024-08-01 18:49:44 +0100

    Change google auth url to env

--- packages/app/app/api/google/request/route.ts
+++ packages/app/app/api/google/request/route.ts
@@ -3,12 +3,14 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const state = searchParams.get('state');
-  const GOOGLE_OAUTH_SECRET = process.env.GOOGLE_OAUTH_SECRET || '';
-  const oAuthSecret: any = JSON.parse(GOOGLE_OAUTH_SECRET);
-  const clientId = oAuthSecret.web.client_id;
-  const redirectUri = oAuthSecret.web.redirect_uri;
+  // const GOOGLE_OAUTH_SECRET = process.env.GOOGLE_OAUTH_SECRET || '';
+  // const oAuthSecret: any = JSON.parse(GOOGLE_OAUTH_SECRET);
+  // const clientId = oAuthSecret.web.client_id;
+  // const redirectUri = oAuthSecret.web.redirect_uri;
 
-  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&prompt=select_account&redirect_uri=${redirectUri}&approval_prompt=force&response_type=code&access_type=offline&scope=https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&state=${state}`;
+  const googleAuthUrl = process.env.GOOGLE_OAUTH_URL;
+
+  const authUrl = `${googleAuthUrl}&state=${state}`;
 
   try {
     return NextResponse.redirect(authUrl);
