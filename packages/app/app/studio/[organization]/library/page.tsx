117046e0020bbdf0adbb2232ed8e118dd2c1fbb2
mariodev <gh_mario.proclaim@slmail.me>
2024-08-13 11:58:41 +0200

    🐛 #853 videos with no assetid giving 404 (#854)
    # 😵 Post-Mortem 😵
    
    ## Summary
    Old videos without `assetId` were not playable due to how we where
    getting the videoUrl.
    
    ## Impact
    -    **Services Affected**: Everywhere we would try to play an old video
    - **User Impact**: user could not see their videos resulting in a very
    frustrating experience
    
    ## Root Cause Analysis
    ```js
    export const getVideoUrlAction = async (assetId: string) => {
      try {
        const asset = await fetchAsset({ assetId });
        if (asset?.playbackUrl) {
          return asset.playbackUrl;
        }
    
    
        return null;
      } catch (e) {
        console.error('Error fetching asset: ', assetId);
        return null;
      }
    };
    ```
    
    This function was not retro-compatible with old videos lacking an
    `assetId`
    
    ## Resolution and Recovery
    Made the function be retro-compatible with old videos
    ```js
    /**
     * Retrieves the video URL for a given identifier, supporting both playbackId and assetId.
     * This function makes all videos playable, including older ones that don't have an assetId.
     *
     * @param {string} identifier - The unique identifier for the video. This can be either a playbackId or an assetId.
     * @param {'assetId' | 'playbackId'} type - Specifies whether the identifier is an 'assetId' or a 'playbackId'.
     * @returns {Promise<string | null>} A promise that resolves to the video URL if successful, or null if unsuccessful.
     *
     * @example
     * // Using with a playbackId
     * const videoUrl = await getVideoUrlAction(session.playbackId, 'playbackId');
     *
     * @example
     * // Using with an assetId
     * const videoUrl = await getVideoUrlAction(session.assetId!, 'assetId');
     *
     * @throws {Error} If there's an issue fetching the asset or building the URL.
     */
    export const getVideoUrlAction = async (
      identifier: string,
      type: 'assetId' | 'playbackId'
    ): Promise<string | null> => {
      try {
        if (type === 'playbackId') {
          // If type is playbackId, simply build the full URL
          return `https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/${identifier}/index.m3u8`;
        } else if (type === 'assetId') {
          // If type is assetId, use the fetch call to retrieve the videoUrl directly
          const asset = await fetchAsset({ assetId: identifier });
          console.log(asset);
          if (asset?.playbackUrl) {
            return asset.playbackUrl;
          }
        }
    
        return null;
      } catch (e) {
        console.error('Error fetching asset or building URL:', identifier);
        return null;
      }
    };
    ```
    
    ## Lessons Learned
    1. When making changes to fetching functions, we need to make sure that
    we take care of legacy DB assets
    2. Test both modern organizations and old ones like Zuzalu to make sure
    that changes do not affect old ones
    
    ## Attachments
    This would before give a 404
    
    ![CleanShot 2024-08-10 at 13 36
    55](https://github.com/user-attachments/assets/66373462-5c25-4e84-9077-e34aab317abf)
    
    This would before not be a playable video
    
    ![CleanShot 2024-08-10 at 13 36
    15](https://github.com/user-attachments/assets/9552d92b-97cf-4347-9b20-a81247b36e81)
    
    ## Additional Notes
    We can discuss my solution in a daily
    
    ## Related Issues
    #853 
    
    **Reviewer Checklist:**
    -    [ ] Incident details are complete
    -    [ ] Root cause is clearly defined
    -    [ ] Resolution steps are clearly detailed
    -    [ ] Related 🐛 issues are provided
    
    closes #853
    
    ---------
    
    Co-authored-by: greatsamist <ifegbenga@gmail.com>

--- packages/app/app/studio/[organization]/library/page.tsx
+++ packages/app/app/studio/[organization]/library/page.tsx
@@ -17,11 +17,6 @@ import Pagination from './components/Pagination';
 import SearchBar from '@/components/misc/SearchBar';
 import LibraryFilter from './components/LibraryFilter';
 import { fetchOrganizationStages } from '@/lib/services/stageService';
-import { fetchAllStates } from '@/lib/services/stateService';
-import {
-  StateStatus,
-  StateType,
-} from 'streameth-new-server/src/interfaces/state.interface';
 
 const Loading = ({ layout }: { layout: string }) => {
   return (
@@ -72,15 +67,6 @@ const Library = async ({
     organizationId: organization._id,
   });
 
-  const statesSet = new Set(
-    (
-      await fetchAllStates({
-        type: StateType.video,
-        status: StateStatus.pending,
-      })
-    ).map((state) => state.sessionId) as unknown as Set<string>
-  );
-
   let sessions = await fetchAllSessions({
     organizationSlug: params.organization,
     limit: searchParams.limit || 20,
@@ -93,7 +79,7 @@ const Library = async ({
   });
 
   let filteredSession = sessions.sessions.filter(
-    (session) => session.videoUrl || statesSet.has(session._id.toString())
+    (session) => session.playbackId
   );
 
   const sortedSessions = sortArray(
