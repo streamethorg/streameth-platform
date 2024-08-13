c24e3d171284b5223679dba2785c42c9182944c4
xvoorvaa <xander@xvproductions.com>
2024-08-13 14:40:10 +0200

    ⚰️: Removed dead code

--- packages/app/playwright.config.ts
+++ packages/app/playwright.config.ts
@@ -46,29 +46,15 @@ export default defineConfig({
       dependencies: ['setup'],
     },
 
-    //{
-    //  name: 'webkit',
-    //  use: { ...devices['Desktop Safari'] },
-    //},
-
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
+    //{
+    //  name: 'Mobile Chrome',
+    //  use: { ...devices['Pixel 5'] },
+    //  dependencies: ['setup'],
+    //},
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
 
