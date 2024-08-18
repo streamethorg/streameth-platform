640d82e381167744feba58bc15012d823d340882
xvoorvaa <xander@xvproductions.com>
2024-08-18 23:05:18 +0200

    Minor removals

--- packages/app/app/speaker/[session]/layout.tsx
+++ packages/app/app/speaker/[session]/layout.tsx
@@ -2,15 +2,12 @@
 
 import HomePageNavbar from '@/components/Layout/HomePageNavbar';
 import Footer from '@/components/Layout/Footer';
-import Support from '@/components/misc/Support';
 import { Page } from '@/lib/types';
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const pages: Page[] = [];
-
   return (
     <div className="flex flex-col mx-auto w-full bg-white min-h-[100vh]">
-      <HomePageNavbar showLogo={true} pages={pages} showSearchBar={false} />
+      <HomePageNavbar showLogo={true} pages={[]} showSearchBar={false} />
       <div className="flex-grow w-full h-full">{children}</div>
       <div className="sticky mb-5 top-[100vh]">
         <Footer />
