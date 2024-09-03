b90aff1d777f56bf29faa1adb139e4ab116b674f
Samuel Ojo <88172184+greatsamist@users.noreply.github.com>
2024-09-03 17:22:43 +0100

    handle transcription state (#923)
    # Pull Request Template
    ## Title
    Handle Video Transcription State with Refresh Icon and Error Handling
    
    ## Type of Change
    - [x] New feature ✨
    - [ ] Bug fix 🐛
    - [ ] Documentation update 📝
    - [ ] Refactoring ♻️
    - [ ] Hotfix 🚑️
    - [ ] UI/UX improvement 💄
    
    ## Description
    
    - Added a processing icon with a refresh icon to indicate when video
    transcription is in progress.
    - Implemented error messaging for failed video transcriptions, including
    a "Try Again" button for retries.
    
    
    ## Checklist
    - [ ] My code adheres to the coding and style guidelines of the project.
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have made corresponding changes to the documentation.
    - [ ] My changes generate no new warnings

--- packages/app/app/studio/[organization]/library/[session]/components/SessionTranscriptions.tsx
+++ packages/app/app/studio/[organization]/library/[session]/components/SessionTranscriptions.tsx
@@ -3,18 +3,24 @@ import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import TextPlaceholder from '@/components/ui/text-placeholder';
 import { generateTranscriptionActions } from '@/lib/actions/sessions';
+import { IExtendedState } from '@/lib/types';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
+import { LuLoader2, LuRefreshCcw } from 'react-icons/lu';
 import { toast } from 'sonner';
 
 const SessionTranscriptions = ({
   videoTranscription,
   organizationId,
   sessionId,
+  transcriptionState,
 }: {
   videoTranscription?: string;
   sessionId: string;
   organizationId: string;
+  transcriptionState: IExtendedState[];
 }) => {
+  const router = useRouter();
   const [isGeneratingTranscript, setIsGeneratingTranscript] = useState(false);
   const handleGenerateTranscription = async () => {
     setIsGeneratingTranscript(true);
@@ -43,9 +49,29 @@ const SessionTranscriptions = ({
         <div>
           <TextPlaceholder text={videoTranscription} />
         </div>
+      ) : transcriptionState[0]?.status === 'pending' ? (
+        <div className="flex items-center">
+          <LuLoader2 className="mr-2 w-4 h-4 animate-spin" /> Processing
+          transcription...{' '}
+          <p className="pl-2" title="refresh" onClick={() => router.refresh()}>
+            <LuRefreshCcw />
+          </p>
+        </div>
+      ) : transcriptionState[0]?.status === 'failed' ? (
+        <div>
+          <p>Transcription failed. Please try again.</p>
+          <Button
+            variant="primary"
+            className="mt-2"
+            onClick={handleGenerateTranscription}
+            loading={isGeneratingTranscript}
+          >
+            Try Again
+          </Button>
+        </div>
       ) : (
         <Button
-          variant={'primary'}
+          variant="primary"
           className="mt-2"
           onClick={handleGenerateTranscription}
           loading={isGeneratingTranscript}
