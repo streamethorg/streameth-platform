e7aee4104dcaa71687ef955b680b7659f1730dde
greatsamist <ifegbenga@gmail.com>
2024-09-04 07:56:19 +0100

    🐛 hotfix: Webhook state fix for sessions

--- packages/server/src/controllers/index.controller.ts
+++ packages/server/src/controllers/index.controller.ts
@@ -104,7 +104,7 @@ export class IndexController extends Controller {
       });
     }
     const state = await this.stateService.findOne({
-      _id: session._id.toString(),
+      sessionId: session._id.toString(),
       type: StateType.video,
     });
     if (!state) throw new HttpException(404, 'No state found');
