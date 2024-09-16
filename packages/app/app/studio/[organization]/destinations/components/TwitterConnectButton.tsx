e0cadf136c909be36a91b6802b8610eeab93f8cf
mariodev <gh_mario.proclaim@slmail.me>
2024-09-16 10:03:31 -0400

    🚧 added the twitter endpoint to the connect to X button
    This took way longer than I would like to admit...

--- packages/app/app/studio/[organization]/destinations/components/TwitterConnectButton.tsx
+++ packages/app/app/studio/[organization]/destinations/components/TwitterConnectButton.tsx
@@ -7,9 +7,11 @@ import { SiX } from 'react-icons/si';
 
 const TwitterConnectButton = ({ state }: { state?: string }) => {
   return (
-    <Button disabled className="min-w-[200px] bg-[#121212]">
-      <SiX className="mr-2" />X account (Coming Soon)
-    </Button>
+    <Link href={`/api/twitter/request?state=${state}`}>
+      <Button className="min-w-[200px] bg-[#121212]">
+        <SiX className="mr-2" />X account
+      </Button>
+    </Link>
   );
 };
 
