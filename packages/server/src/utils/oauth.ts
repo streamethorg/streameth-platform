95f304f32e7767b5c5f33d8fcbe619b2384d29cb
Eric-Vondee <ericvondee5@gmail.com>
2024-10-02 23:18:46 +0100

    ✨ add validate auth token

--- packages/server/src/utils/oauth.ts
+++ packages/server/src/utils/oauth.ts
@@ -26,3 +26,37 @@ export const refreshAccessToken = async (
   const token = await oauth2Client.refreshAccessToken();
   return token.credentials.access_token;
 };
+
+export const validateToken = async (
+  token: string,
+): Promise<{ isValid: boolean; userId: string; email: string }> => {
+  try {
+    const oauth2Client = new google.auth.OAuth2(
+      config.oauth.google.clientId,
+      config.oauth.google.secretKey,
+    );
+    const tokens = await oauth2Client.verifyIdToken({
+      idToken: token,
+      audience: config.oauth.google.clientId,
+    });
+    const payload = tokens.getPayload();
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (payload.exp < currentTime) {
+      throw new Error('Token expired');
+    }
+    return {
+      isValid: true,
+      userId: payload['sub'],
+      email: payload['email'],
+    };
+  } catch (e) {
+    if (e instanceof Error) {
+      if (e.message.includes('Invalid token signature')) {
+        throw new Error('Invalid or expired token');
+      }
+      if(e.message.includes('Token used too late')){
+        throw new Error('Token expired');
+      }
+    }
+  }
+};
