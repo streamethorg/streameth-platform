055536988ff079c7cb279dc908d20c70f88e11ac
Eric-Vondee <ericvondee5@gmail.com>
2024-10-09 06:39:49 +0100

    ✨ auth service refactor

--- packages/server/src/services/auth.service.ts
+++ packages/server/src/services/auth.service.ts
@@ -1,41 +1,61 @@
 import { config } from '@config';
 import { HttpException } from '@exceptions/HttpException';
+import { AuthType, IAuth } from '@interfaces/auth.interface';
 import { IUser } from '@interfaces/user.interface';
+import EmailService from '@utils/mail.service';
 import { validateToken } from '@utils/oauth';
+import { replacePlaceHolders } from '@utils/util';
+import crypto from 'crypto';
+import fs from 'fs';
 import jwt from 'jsonwebtoken';
+import path from 'path';
 import UserService from './user.service';
 
 export default class AuthService {
   private userService = new UserService();
-  private path: string;
-  constructor() {
-    this.path = 'auth';
-  }
+  private mailService = new EmailService();
 
-  async login(data: IUser): Promise<{ user: IUser; token: string }> {
-    let verifyToken = await this.verifyAuthToken(data.token);
-    let user = await this.userService.findOne({ email: verifyToken.email });
+  async login(data: IAuth): Promise<{ user: IUser; token: string }> {
+    let verifyToken = await this.verifyAuthToken({
+      token: data.token,
+      type: data.type,
+      email: data.email,
+    });
+    let user = await this.userService.findOne({
+      email: verifyToken.email,
+    });
     if (!user) {
       user = await this.userService.create({
         email: verifyToken.email,
         did: verifyToken.userId,
       });
     }
-    let token = jwt.sign({ id: verifyToken.userId }, config.jwt.secret, {
+    let token = jwt.sign({ id: user.did }, config.jwt.secret, {
       expiresIn: config.jwt.expiry,
     });
     return { user, token };
   }
 
-  async verifyAuthToken(
-    token: string,
-  ): Promise<{ userId: string; email: string }> {
+  async verifyAuthToken(d: {
+    token: string;
+    type: string;
+    email?: string;
+  }): Promise<{ userId: string; email: string }> {
     try {
-      const authToken = await validateToken(token);
-      return {
-        userId: authToken.userId,
-        email: authToken.email,
-      };
+      if (d.type === AuthType.email) {
+        await this.verifyToken(d.token);
+        return {
+          userId: '',
+          email: d.email,
+        };
+      }
+      if (d.type === AuthType.google) {
+        const authToken = await validateToken(d.token);
+        return {
+          userId: authToken.userId,
+          email: authToken.email,
+        };
+      }
     } catch (e) {
       if (e.message === 'Invalid or expired token') {
         throw new HttpException(401, 'Invalid token');
@@ -54,7 +74,7 @@ export default class AuthService {
         e instanceof jwt.TokenExpiredError ||
         e instanceof jwt.JsonWebTokenError
       ) {
-        throw new HttpException(401, 'Invalid token');
+        throw new HttpException(401, 'Token expired');
       }
       throw new HttpException(401, 'Invalid token');
     }
@@ -74,5 +94,28 @@ export default class AuthService {
         throw new HttpException(401, 'Invalid token');
       }
     }
+  }
+
+  async generateMagicLink(email: string): Promise<void> {
+    const emailTemplate = fs.readFileSync(
+      path.join(__dirname, 'templates/login.html'),
+      'utf8',
+    );
+    const token = jwt.sign({ id: crypto.randomUUID() }, config.jwt.secret, {
+      expiresIn: '1d',
+    });
+    const htmlContent = replacePlaceHolders(emailTemplate, {
+      invitationLink: `${config.baseUrl}/auth/signin?token=${token}`,
+    });
+    const user = await this.userService.findOne({ email });
+    if (!user) {
+      await this.userService.create({ email, did: crypto.randomUUID() });
+    }
+    await this.mailService.simpleSend({
+      from: '',
+      recipient: email,
+      subject: 'Login to Streameth',
+      html: htmlContent,
+    });
   }
 }
