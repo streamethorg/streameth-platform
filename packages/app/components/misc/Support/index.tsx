ace60175fb5ce70d30d49d04e28331176dca9185
mariodev <gh_mario.proclaim@slmail.me>
2024-10-15 08:52:53 -0400

    🔥 Commented out the support Credenza component
    This was discussed in a meeting. The support floating form was not
    letting users properly access the delete button for the last marker in
    the list

--- packages/app/components/misc/Support/index.tsx
+++ packages/app/components/misc/Support/index.tsx
@@ -50,54 +50,54 @@ const Support = () => {
       });
   }
 
-  return (
-    <Credenza open={open} onOpenChange={setOpen}>
-      <CredenzaContent className="max-h-[700px] p-6 md:overflow-auto">
-        <CredenzaTitle>Contact Support</CredenzaTitle>
-        <div>
-          {messageSent && <p className="pb-2 font-bold">Message sent 🎉!!</p>}
-          <p className="pb-2">
-            Feel free to become a part of our{' '}
-            <a
-              className="font-semibold text-primary"
-              href="https://t.me/+p7TgdE06G-4zZDU0"
-              target="_blank"
-              rel="noopener noopener"
-            >
-              Telegram support
-            </a>{' '}
-            channel to stay informed about ticket updates, raise an issue on{' '}
-            <a
-              className="font-semibold text-primary"
-              href="https://github.com/streamethorg/streameth-platform/issues"
-              target="_blank"
-              rel="noopener noopener"
-            >
-              GitHub{' '}
-            </a>
-            or even send an email to{' '}
-            <a
-              href="mailto:support@streameth.com"
-              className="font-semibold text-primary"
-            >
-              support@streameth.com
-            </a>
-          </p>
-        </div>
-      </CredenzaContent>
-      <CredenzaTrigger>
-        <div className="fixed bottom-4 right-0 mr-1 cursor-pointer p-1 lg:ml-0 lg:p-2">
-          <div className="rounded-full bg-white p-4 shadow-xl">
-            {!open ? (
-              <MessageCircleQuestion className="h-7 w-7 text-muted-foreground" />
-            ) : (
-              <X className="h-7 w-7 text-muted-foreground" />
-            )}
-          </div>
-        </div>
-      </CredenzaTrigger>
-    </Credenza>
-  );
+  // return (
+  // <Credenza open={open} onOpenChange={setOpen}>
+  //   <CredenzaContent className="max-h-[700px] p-6 md:overflow-auto">
+  //     <CredenzaTitle>Contact Support</CredenzaTitle>
+  //     <div>
+  //       {messageSent && <p className="pb-2 font-bold">Message sent 🎉!!</p>}
+  //       <p className="pb-2">
+  //         Feel free to become a part of our{' '}
+  //         <a
+  //           className="font-semibold text-primary"
+  //           href="https://t.me/+p7TgdE06G-4zZDU0"
+  //           target="_blank"
+  //           rel="noopener noopener"
+  //         >
+  //           Telegram support
+  //         </a>{' '}
+  //         channel to stay informed about ticket updates, raise an issue on{' '}
+  //         <a
+  //           className="font-semibold text-primary"
+  //           href="https://github.com/streamethorg/streameth-platform/issues"
+  //           target="_blank"
+  //           rel="noopener noopener"
+  //         >
+  //           GitHub{' '}
+  //         </a>
+  //         or even send an email to{' '}
+  //         <a
+  //           href="mailto:support@streameth.com"
+  //           className="font-semibold text-primary"
+  //         >
+  //           support@streameth.com
+  //         </a>
+  //       </p>
+  //     </div>
+  //   </CredenzaContent>
+  //   <CredenzaTrigger>
+  //     <div className="fixed bottom-4 right-0 mr-1 cursor-pointer p-1 lg:ml-0 lg:p-2">
+  //       <div className="rounded-full bg-white p-4 shadow-xl">
+  //         {!open ? (
+  //           <MessageCircleQuestion className="h-7 w-7 text-muted-foreground" />
+  //         ) : (
+  //           <X className="h-7 w-7 text-muted-foreground" />
+  //         )}
+  //       </div>
+  //     </div>
+  //   </CredenzaTrigger>
+  // </Credenza>
+  // );
 };
 
 export default Support;
