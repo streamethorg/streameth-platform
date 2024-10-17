bd21e9c35913e8cf68a9fda4817d338333eb8477
mariodev <gh_mario.proclaim@slmail.me>
2024-10-17 10:41:32 +0200

    Markers sidebar broken again (#1028)
    ## Type of Change
    
    - [x] Bug fix 🐛
    
    ## Description
    fixed #989 again, might have been overwritten somehow + removed the
    floating support form.
    
    ## Testing
    Manual testing as seen in the video
    
    ## Additional Information
    
    
    https://github.com/user-attachments/assets/6d0e1b76-63b7-4eec-a99c-d3ec51c61217
    
    ## Checklist
    - [ ] My code adheres to the coding and style guidelines of the project.
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have made corresponding changes to the documentation.
    - [ ] My changes generate no new warnings
    
    ---------
    
    Co-authored-by: greatsamist <ifegbenga@gmail.com>

--- packages/app/app/studio/[organization]/clips/[stageId]/sidebar/index.tsx
+++ packages/app/app/studio/[organization]/clips/[stageId]/sidebar/index.tsx
@@ -37,13 +37,13 @@ export default function Sidebar({
   ];
 
   return (
-    <div className="h-full w-full border-l bg-white relative">
+    <div className="h-full w-full border-l bg-white relative flex flex-col">
       {overlayComponents.map(
         ({ condition, Component, usesLiveRecordingId }) =>
           condition && (
             <div
               key={Component.name}
-              className="h-full w-full border-l bg-white absolute z-50"
+              className="absolute inset-0 z-50 bg-white"
             >
               <Component
                 organizationId={organizationId}
@@ -52,15 +52,15 @@ export default function Sidebar({
             </div>
           )
       )}
-      <Tabs defaultValue="clips" className=" h-full ">
-        <TabsList className="grid w-full grid-cols-2">
+      <Tabs defaultValue="clips" className="flex flex-col h-full">
+        <TabsList className="grid w-full grid-cols-2 flex-shrink-0">
           <TabsTrigger value="markers">Markers</TabsTrigger>
           <TabsTrigger value="clips">Clips</TabsTrigger>
         </TabsList>
-        <TabsContent value="markers" className="h-full">
+        <TabsContent value="markers" className="flex-grow overflow-hidden">
           {<Markers organizationId={organizationId} />}
         </TabsContent>
-        <TabsContent value="clips" className="h-full">
+        <TabsContent value="clips" className="flex-grow overflow-hidden">
           <SessionSidebar sessions={stageSessions} />
         </TabsContent>
       </Tabs>
