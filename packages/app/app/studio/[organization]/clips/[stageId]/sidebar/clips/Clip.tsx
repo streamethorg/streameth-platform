3b39df08c396912fed923d54b1c396e11cb31e61
greatsamist <ifegbenga@gmail.com>
2024-10-24 02:20:13 +0100

    fix video playback delay after processing and handle processing failures

--- packages/app/app/studio/[organization]/clips/[stageId]/sidebar/clips/Clip.tsx
+++ packages/app/app/studio/[organization]/clips/[stageId]/sidebar/clips/Clip.tsx
@@ -8,13 +8,15 @@ import Preview from './Preview';
 import { Asset } from 'livepeer/models/components';
 import { fetchAsset } from '@/lib/services/sessionService';
 import useGenerateThumbnail from '@/lib/hooks/useGenerateThumbnail';
+import { ProcessingStatus } from 'streameth-new-server/src/interfaces/session.interface';
+import { useRouter } from 'next/navigation';
 
 export default function Clip({ session }: { session: IExtendedSession }) {
   const { name, coverImage, assetId } = session;
   const [asset, setAsset] = useState<Asset | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const thumbnail = useGenerateThumbnail({ session });
-
+  const router = useRouter();
   const getAsset = async () => {
     if (assetId) {
       const asset = await fetchAsset({
@@ -35,6 +37,11 @@ export default function Clip({ session }: { session: IExtendedSession }) {
         getAsset();
       }, 10000);
       return () => clearInterval(interval);
+    } else if (session.processingStatus === ProcessingStatus.pending) {
+      const interval = setInterval(() => {
+        router.refresh();
+      }, 10000);
+      return () => clearInterval(interval);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [asset?.status?.phase]);
@@ -43,7 +50,8 @@ export default function Clip({ session }: { session: IExtendedSession }) {
   return (
     <>
       <div>
-        {asset?.status?.phase === 'processing' ? (
+        {asset?.status?.phase === 'processing' ||
+        session.processingStatus === ProcessingStatus.pending ? (
           <Card className="w-full cursor-not-allowed animate-pulse max-w-2xl overflow-hidden px-2 py-1 shadow-none bg-muted">
             <div className="flex justify-center items-center">
               <div className="flex-shrink-0 w-1/3">
@@ -55,10 +63,12 @@ export default function Clip({ session }: { session: IExtendedSession }) {
                   Video is processing...
                 </p>
                 <div className="flex items-center justify-between gap-2">
-                  <p className="text-sm text-gray-500 mt-1">
-                    {Math.round(Number(asset?.status?.progress ?? 0) * 100)}%
-                    complete.
-                  </p>
+                  {asset?.status?.phase === 'processing' && (
+                    <p className="text-sm text-gray-500 mt-1">
+                      {Math.round(Number(asset?.status?.progress ?? 0) * 100)}%
+                      complete.
+                    </p>
+                  )}
                   <p className="text-[11px] text-gray-500 mt-1">
                     {formatDate(
                       new Date(session.createdAt as string),
@@ -69,6 +79,30 @@ export default function Clip({ session }: { session: IExtendedSession }) {
               </CardContent>
             </div>
           </Card>
+        ) : session.processingStatus === ProcessingStatus.failed ? (
+          <div>
+            <Card className="w-full cursor-not-allowed border border-destructive max-w-2xl overflow-hidden px-2 py-1 shadow-none bg-muted">
+              <div className="flex justify-center items-center">
+                <div className="flex-shrink-0 w-1/3">
+                  <Thumbnail imageUrl={coverImage} fallBack={thumbnail} />
+                </div>
+                <CardContent className="lg:p-2 p-2 flex-grow">
+                  <h2 className="text-lg font-semibold line-clamp-1">{name}</h2>
+                  <p className="text-sm text-destructive mt-1">
+                    Video processing failed
+                  </p>
+                  <div className="flex items-center justify-between gap-2">
+                    <p className="text-[11px] text-gray-500 mt-1">
+                      {formatDate(
+                        new Date(session.createdAt as string),
+                        'ddd. MMM. D, YYYY'
+                      )}
+                    </p>
+                  </div>
+                </CardContent>
+              </div>
+            </Card>
+          </div>
         ) : (
           <Card
             onClick={() => setIsOpen(true)}
