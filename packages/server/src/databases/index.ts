7c3e447490c7f80719f15797760a09f5f09b286b
Mario-SO <gh_mario.proclaim@slmail.me>
2024-12-11 12:35:59 +0100

    🚑 commented the srv protocol link

--- packages/server/src/databases/index.ts
+++ packages/server/src/databases/index.ts
@@ -1,7 +1,9 @@
 import { config } from '@config';
 const { host, user, name, password } = config.db;
 export const dbConnection = {
-  url: `mongodb+srv://${user}:${password}@${host}/${name}?authSource=admin`,
+  url: `mongodb://${user}:${password}@${host}/${name}?authSource=admin`,
+  // For local development use this url
+  // url: `mongodb+srv://${user}:${password}@${host}/${name}?authSource=admin`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
