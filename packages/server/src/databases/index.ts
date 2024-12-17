2ae58e9e7c9f68a1501b4e69ea89739a2a0b2ee5
Mario-SO <gh_mario.proclaim@slmail.me>
2024-12-17 21:05:26 +0100

    🔊 DB debug

--- packages/server/src/databases/index.ts
+++ packages/server/src/databases/index.ts
@@ -1,11 +1,13 @@
 import { config } from '@config';
 const { host, user, name, password } = config.db;
 export const dbConnection = {
-  url: `mongodb://${user}:${password}@${host}/${name}?authSource=admin`,
+  url: `mongodb://${user}:${password}@${host}/${name}?authSource=admin&retryWrites=true&w=majority`,
   // For local development use this url
   // url: `mongodb+srv://${user}:${password}@${host}/${name}?authSource=admin`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
   },
 };
