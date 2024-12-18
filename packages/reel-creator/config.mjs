be7e0f62651af177f2a5b6dcb8bf9143d59e7de5
Mario-SO <gh_mario.proclaim@slmail.me>
2024-12-18 11:04:48 +0100

    🐛 Possible misspelling

--- packages/reel-creator/config.mjs
+++ packages/reel-creator/config.mjs
@@ -1,5 +1,11 @@
 import { VERSION } from "remotion/version";
 
+console.log('Raw environment variables in config.mjs:');
+console.log('SERVER_WEBHOOK_URL:', process.env.SERVER_WEBHOOK_URL);
+console.log('SERVER_WEBHOOK_SECRET_FILE:', process.env.SERVER_WEBHOOK_SECRET_FILE);
+console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
+
 /**
  * Use autocomplete to get a list of available regions.
  * @type {import('@remotion/lambda').AwsRegion}
@@ -13,5 +19,5 @@ export const TIMEOUT = 900;
 
 export const WEBHOOK_URL = process.env.SERVER_WEBHOOK_URL;
 export const WEBHOOK_SECRET = process.env.SERVER_WEBHOOK_SECRET_FILE;
-export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID_FILE;
-export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY_FILE;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
