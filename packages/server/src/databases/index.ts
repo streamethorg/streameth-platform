b9e69fd88c655576baa3057c3a3acd6507c4a67b
Mario-SO <gh_mario.proclaim@slmail.me>
2024-12-19 11:28:45 +0100

    fixed db password (ai suggestion)

--- packages/server/src/databases/index.ts
+++ packages/server/src/databases/index.ts
@@ -1,7 +1,7 @@
 import { config } from '@config';
 const { host, user, name, password } = config.db;
 export const dbConnection = {
-  url: `mongodb://${user}:${password}@${host}/${name}?authSource=admin&retryWrites=true&w=majority`,
+  url: `mongodb://${user}:${encodeURIComponent(password)}@${host}/${name}?authSource=admin&retryWrites=true&w=majority`,
   // For local development use this url
   // url: `mongodb+srv://${user}:${password}@${host}/${name}?authSource=admin`,
   options: {
