1413abba5a0b524df83892d86ecdf83e58934093
Mario-SO <gh_mario.proclaim@slmail.me>
2024-12-19 18:46:32 +0100

    explicit node env for server and mongodb

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -39,7 +39,7 @@ class App {
   listen() {
     this.app.listen(this.port, () => {
       logger.info(`=================================`);
-      logger.info(`======= ENV: ${this.env} =======`);
+      logger.info(`======= ENV: ${this.env} ========`);
       logger.info(`🚀 App listening on the port ${this.port}`);
       logger.info(`=================================`);
     });
