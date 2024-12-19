b808c0e05b1377e9116133f6dbd4f7162e88168a
Mario-SO <gh_mario.proclaim@slmail.me>
2024-12-19 09:39:53 +0100

    read from /sercrets in config

--- packages/reel-creator/config.mjs
+++ packages/reel-creator/config.mjs
@@ -10,6 +10,19 @@ console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
  * Use autocomplete to get a list of available regions.
  * @type {import('@remotion/lambda').AwsRegion}
  */
+
+const readSecretFile = (path) => {
+    if (process.env.NODE_ENV === 'development') {
+      return path;
+    }
+    try {
+      return fs.readFileSync(path, 'utf8').trim();
+    } catch (error) {
+      console.error(`Error reading secret file ${path}:`, error);
+      throw error;
+    }
+  };
+
 export const REGION = "us-east-2";
 
 export const SITE_NAME = 'rendering-engine';
@@ -18,6 +31,7 @@ export const DISK = 10240;
 export const TIMEOUT = 900;
 
 export const WEBHOOK_URL = process.env.SERVER_WEBHOOK_URL;
-export const WEBHOOK_SECRET = process.env.SERVER_WEBHOOK_SECRET_FILE;
-export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const WEBHOOK_SECRET = readSecretFile(process.env.SERVER_WEBHOOK_SECRET_FILE);
+export const AWS_ACCESS_KEY_ID = readSecretFile(process.env.AWS_ACCESS_KEY_ID_FILE);
+export const AWS_SECRET_ACCESS_KEY = readSecretFile(process.env.AWS_SECRET_ACCESS_KEY_FILE);
+
