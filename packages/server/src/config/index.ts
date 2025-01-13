d77b5353b298398a86b0d0541d7101a050ff1e09
Mario-SO <gh_mario.proclaim@slmail.me>
2025-01-13 17:49:37 +0100

    ⚙️ Stripe config

--- packages/server/src/config/index.ts
+++ packages/server/src/config/index.ts
@@ -94,4 +94,8 @@ export const config = {
     port: validatedEnv.REDIS_PORT,
     password: readSecretFile(validatedEnv.REDIS_PASSWORD_FILE),
   },
+  stripe: {
+    apiKey: readSecretFile(validatedEnv.STRIPE_API_KEY_FILE),
+    publishableKey: readSecretFile(validatedEnv.STRIPE_PUBLISHABLE_KEY_FILE),
+  },
 };
