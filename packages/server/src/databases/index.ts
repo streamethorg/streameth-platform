a0a7f5210976ab4cdaf22fd6c4e3ba69c32052f8
Mario-SO <gh_mario.proclaim@slmail.me>
2025-01-19 18:58:21 +0100

    🚑 Classic DB issue

--- packages/server/src/databases/index.ts
+++ packages/server/src/databases/index.ts
@@ -2,9 +2,9 @@ import { config } from '@config';
 const { host, user, name, password } = config.db;
 
 export const dbConnection = {
-  //url: `mongodb://${user}:${password}@${host}/${name}?authSource=admin&retryWrites=true&w=majority`,
+  url: `mongodb://${user}:${password}@${host}/${name}?authSource=admin&retryWrites=true&w=majority`,
   // For local development use this url
-  url: `mongodb+srv://${user}:${password}@${host}/${name}?authSource=admin`,
+  // url: `mongodb+srv://${user}:${password}@${host}/${name}?authSource=admin`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
