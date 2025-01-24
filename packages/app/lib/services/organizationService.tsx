6557d894dd6a81e4b43fe69a488027afa943639a
Mario-SO <gh_mario.proclaim@slmail.me>
2025-01-24 17:05:59 +0100

    ⚙️ Added cache: no store and revalidate to org page

--- packages/app/lib/services/organizationService.tsx
+++ packages/app/lib/services/organizationService.tsx
@@ -18,7 +18,11 @@ export async function fetchOrganization({
     const response = await fetch(
       `${apiUrl()}/organizations/${
         organizationId ? organizationId : organizationSlug
-      }`
+      }`,
+      {
+        cache: 'no-store',
+        next: { revalidate: 0 }
+      }
     );
     const data = (await response.json()).data;
 
