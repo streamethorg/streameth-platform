03318012d6b39bb297ad41d82ca8bdb2d3971bb9
Mario-SO <gh_mario.proclaim@slmail.me>
2025-02-07 12:04:29 +0100

    🚑 Path changes hotfix

--- packages/server/src/services/auth.service.ts
+++ packages/server/src/services/auth.service.ts
@@ -116,7 +116,7 @@ export default class AuthService {
 
   async generateMagicLink(email: string): Promise<void> {
     const emailTemplate = fs.readFileSync(
-      path.join(process.cwd(), 'templates', 'login.html'),
+      path.join(__dirname, '../../../templates/login.html'),
       'utf8'
     );
     const token = jwt.sign(
