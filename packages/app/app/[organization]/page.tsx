63061de7cae7224de2c866d7c6ff491a671991b1
xannyx <xander@xvproductions.com>
2025-05-27 14:22:09 +0200

    Changed naming of Upcoming stream to just Live & showing stream if live

--- packages/app/app/[organization]/page.tsx
+++ packages/app/app/[organization]/page.tsx
@@ -1,10 +1,10 @@
+"use server";
+
 import { Metadata, ResolvingMetadata } from "next";
 import { fetchOrganization } from "@/lib/services/organizationService";
 import { ChannelPageParams } from "@/lib/types";
 import { Suspense } from "react";
-import UpcomingStreams, {
-	UpcomingStreamsLoading,
-} from "./components/UpcomingStreams";
+import LiveStreams, { LiveStreamsLoading } from "./components/UpcomingStreams";
 import { fetchOrganizationStages } from "@/lib/services/stageService";
 import { livestreamMetadata, generalMetadata } from "@/lib/utils/metadata";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
@@ -14,18 +14,28 @@ import ChannelBanner from "./components/ChannelBanner";
 import ArchivePlaylists, {
 	PlaylistSkeleton,
 } from "./playlists/components/ArchivePlaylists";
+import Player from "./livestream/components/Player";
 
 const OrganizationHome = async ({
 	params,
 	searchParams,
 }: ChannelPageParams) => {
+	const streams = await fetchOrganizationStages({
+		organizationId: params.organization,
+	});
+	const defaultTabValue = streams.length > 0 ? "livestreams" : "videos";
+
+	const liveStream = streams.find(
+		(stream) => stream.streamSettings?.isActive === true,
+	);
+
 	return (
 		<div className="mx-auto space-y-4 w-full max-w-7xl md:p-4">
-			<ChannelBanner />
-			<Tabs defaultValue="videos" className="w-full">
+			{liveStream ? <Player stage={liveStream} /> : <ChannelBanner />}
+			<Tabs defaultValue={defaultTabValue} className="w-full">
 				<TabsList className="justify-start mb-2 w-full">
 					<TabsTrigger value="livestreams">
-						<h1 className="text-xl font-bold">Upcoming Streams</h1>
+						<h1 className="text-xl font-bold">Live</h1>
 					</TabsTrigger>
 					<TabsTrigger value="videos">
 						<h1 className="text-xl font-bold">Videos</h1>
@@ -35,8 +45,8 @@ const OrganizationHome = async ({
 					</TabsTrigger>
 				</TabsList>
 				<TabsContent value="livestreams" className="px-4 md:px-0">
-					<Suspense fallback={<UpcomingStreamsLoading />}>
-						<UpcomingStreams
+					<Suspense fallback={<LiveStreamsLoading />}>
+						<LiveStreams
 							organizationId={params.organization}
 							currentStreamId={searchParams.streamId}
 						/>
