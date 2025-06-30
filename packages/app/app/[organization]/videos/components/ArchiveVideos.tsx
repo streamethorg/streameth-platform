77cef066dd97520f97e106dde7de41b7b1bd373f
xannyx <xander@xvproductions.com>
2025-06-30 16:05:32 +0200

    Randomized sessions array to show new suggestions on watch page

--- packages/app/app/[organization]/videos/components/ArchiveVideos.tsx
+++ packages/app/app/[organization]/videos/components/ArchiveVideos.tsx
@@ -1,49 +1,60 @@
-import { fetchAllSessions } from '@/lib/services/sessionService';
-import VideoGrid from '@/components/misc/Videos';
-import { FileQuestion } from 'lucide-react';
-import Pagination from '@/app/studio/[organization]/(root)/library/components/Pagination';
+import { fetchAllSessions } from "@/lib/services/sessionService";
+import VideoGrid from "@/components/misc/Videos";
+import { FileQuestion } from "lucide-react";
+import Pagination from "@/app/studio/[organization]/(root)/library/components/Pagination";
+import shuffleArray from "@/lib/utils/shuffleArray";
 
 interface ArchiveVideosProps {
-  organizationId?: string;
-  organizationSlug?: string;
-  searchQuery?: string;
-  page?: number;
-  gridLength: number;
+	organizationId?: string;
+	organizationSlug?: string;
+	searchQuery?: string;
+	page?: number;
+	gridLength: number;
 }
 
 const ArchiveVideos = async ({
-  organizationId,
-  organizationSlug,
-  searchQuery,
-  page,
-  gridLength = 10,
+	organizationId,
+	searchQuery,
+	page,
+	gridLength = 10,
 }: ArchiveVideosProps) => {
-  const { sessions: videos, pagination } = await fetchAllSessions({
-    organizationId,
-    limit: gridLength,
-    onlyVideos: true,
-    published: 'public',
-    searchQuery,
-    page,
-  });
+	const { sessions, pagination } = await fetchAllSessions({
+		organizationId,
+		limit: gridLength,
+		onlyVideos: true,
+		published: "public",
+		searchQuery,
+		page,
+	});
 
-  if (videos.length === 0) {
-    return (
-      <div className="mt-10 flex h-full w-full flex-col items-center justify-center">
-        <FileQuestion size={65} />
-        <span className="bolt mt-2 text-xl">
-          No videos have been uploaded yet
-        </span>
-      </div>
-    );
-  }
+	let shuffledSessions = shuffleArray(sessions);
 
-  return (
-    <>
-      <VideoGrid videos={videos} />
-      {page && pagination.totalPages > 1 && <Pagination {...pagination} />}
-    </>
-  );
+	if (!searchQuery && sessions.length === 0) {
+		return (
+			<div className="flex flex-col justify-center items-center mt-10 w-full h-full">
+				<FileQuestion size={65} />
+				<span className="mt-2 text-xl bolt">
+					No videos have been uploaded yet
+				</span>
+			</div>
+		);
+	}
+
+	if (searchQuery && sessions.length === 0) {
+		return (
+			<div className="flex flex-col justify-center items-center mt-10 w-full h-full">
+				<FileQuestion size={65} />
+				<span className="mt-2 text-xl bolt">No videos have been found</span>
+			</div>
+		);
+	}
+
+	return (
+		<>
+			<VideoGrid videos={shuffledSessions} />
+			{page && pagination.totalPages > 1 && <Pagination {...pagination} />}
+		</>
+	);
 };
 
 export default ArchiveVideos;
